a263b99f3af2487c18f5403e3a28b568ded426fe
richardpope <richardpope@richard-popes-macbook-pro-3.local>
2009-10-23 15:15:19 +0100

    basic styling for teh editor + julians resizing code

--- web/media/js/editor.js
+++ web/media/js/editor.js
@@ -23,6 +23,10 @@ $(document).ready(function()
          $('#console').replaceWith('<iframe name="console" id="console" style="width:100%"></iframe>')
        })
        
+       //setup dragging
+       $("#resizebar").bind("mousedown", startDrag);   // see below
+       
+       
        // $("#editor #run_script").click(function() {  
        //   // alert('asd')
        //    $('#editor').attr({
@@ -37,3 +41,41 @@ $(document).ready(function()
        // });  
        
     });
+
+    // dragging of middle bar code -- could move into own javascript library
+	var staticOffset;  
+	var iLastMousePos = 0;
+    var draggedWindow; 
+    function mouseY(e) 
+    { 
+        return e.clientY + document.documentElement.scrollTop; 
+    };
+
+    function startDrag(e){
+        iLastMousePos = mouseY(e);
+        draggedWindow = $("#id_code").next().children(":first"); // this gets you to the iframe that you need to be resizing!!
+        staticOffset = draggedWindow.height() - iLastMousePos;
+        $(document).mousemove(performDrag).mouseup(endDrag);
+    }
+    function performDrag(e) 
+    {        
+        var iThisMousePos = mouseY(e);
+        var iMousePos = staticOffset + iThisMousePos;
+        if (iLastMousePos >= (iThisMousePos)) 
+            iMousePos -= 5;
+        iLastMousePos = iThisMousePos;
+        //$("#Dout").text("mouse drag: " + iThisMousePos)
+        iMousePos = Math.max(60, iMousePos);
+        draggedWindow.height(iMousePos + 'px');
+        return false;
+        
+        //TODO: scrollbars should never appear on this page - the output section
+        //should be squished, and dragging stopped when only the buttons are visible
+    }
+    function endDrag(e) 
+    {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        staticOffset = null;
+        iLastMousePos = 0;
+        draggedWindow = null; 
+    }
