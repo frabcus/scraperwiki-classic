0637e39e69758bfbd83a07119c785a5ff93c270c
richardpope <richardpope@unknown-00-22-41-1f-18-51.config>
2009-11-09 14:39:33 +0000

    download as csv

--- web/scraper/urls.py
+++ web/scraper/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
                        url(r'^create/$', views.create, name='scraper_create'),
                        url(r'^show/(?P<scraper_short_name>[\w_\-]+)/$', views.data, name='scraper_data'),
                        url(r'^show/(?P<scraper_short_name>[\w_\-]+)/code/$', views.code, name='scraper_code'),
-                       url(r'^download/(?P<scraper_id>[\w_\-]+)$', views.download, name='scraper_download'),
+                       url(r'^download/(?P<scraper_short_name>[\w_\-]+)/$', views.download, name='scraper_download'),
+                       url(r'^export/(?P<scraper_short_name>[\w_\-]+)/$', views.export_csv, name='export_csv'),                       
                        url(r'^request/$', views.scraper_request, name='scraper_request'),)
 
