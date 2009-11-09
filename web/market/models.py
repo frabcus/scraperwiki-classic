07c65997b662fc6c03c3923f9d1eb06183c4e709
richardpope <richardpope@unknown-00-22-41-1f-18-51.config>
2009-11-09 15:17:59 +0000

    market stuff

--- web/market/models.py
+++ web/market/models.py
@@ -10,7 +10,6 @@ class Solicitation(models.Model):
     title   = models.CharField(max_length = 100, null=False, blank=False)
     details = models.TextField(null=False, blank=False)
     link    = models.URLField(verify_exists=True, max_length=200, null=False, blank=False)
-    price   = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add = True)
     deleted = models.BooleanField()
     status  = models.CharField(max_length = 10)
