e576753e18758fbee1dc0046a8f042b5349723d5
goatchurch <goatchurch@ubuntu.clocksoft.dom>
2009-11-20 19:15:19 +0000

    add in an undocumented datastor.load function

--- scraperlibs/scraperwiki/datastore/save.py
+++ scraperlibs/scraperwiki/datastore/save.py
@@ -30,7 +30,11 @@ def save(unique_keys, data=None, **kwargs):
   else:
     return __save_row(unique_keys, data, kwargs)
 
-    
+
+
+# slight issue with this method in case unique_keys are ever a different subset
+# we're almost getting the the realm where they should be part of their own table
+# with the other key values hanging off them
 def __create_unique(unique_keys, data):
   data_set = set(data)
   unique_keys = set(unique_keys)
@@ -45,6 +49,7 @@ def __create_unique(unique_keys, data):
   unique_values = [str(data[v]) for v in unique_keys]
   return hashlib.md5("%s" % ("≈≈≈".join(unique_values))).hexdigest()
 
+indexed_rows = ['date', 'latlng']  # global
 
 def __save_row(unique_keys, data, kwargs):
   """
@@ -56,12 +61,10 @@ def __save_row(unique_keys, data, kwargs):
     DUMMY_RUN = False
   
   # Add all the kwargs in to data
-  data.update(kwargs)
+  data.update(kwargs)   # merges two dicts
 
   # Create a unique hash
   unique_hash = __create_unique(unique_keys, data)
-  
-  indexed_rows = ['date', 'latlng']
   
   for k in indexed_rows:
     if k not in data.keys():
@@ -78,9 +81,10 @@ def __save_row(unique_keys, data, kwargs):
   if not DUMMY_RUN:
     conn = connection.Connection()
     c = conn.connect()
-    if c.execute("SELECT item_id FROM items WHERE unique_hash='%s'" % unique_hash):  
-      item_id = c.fetchone()
-      c.execute("DELETE FROM kv WHERE item_id=%s" % item_id[0])
+    
+    if c.execute("SELECT item_id FROM items WHERE unique_hash=%s", (unique_hash,)):  
+      item_id = c.fetchone()[0]
+      c.execute("DELETE FROM kv WHERE item_id=%s", (item_id,))
       c.execute("DELETE FROM items WHERE unique_hash=%s", (unique_hash,))
   
     c.execute("UPDATE sequences SET id=LAST_INSERT_ID(id+1);")
@@ -94,23 +98,65 @@ def __save_row(unique_keys, data, kwargs):
     str_now = now.strftime("%Y-%m-%d %H:%M:%S")
     
     c.execute("INSERT INTO `items` (`scraper_id`,`item_id`,`unique_hash`,`date`, `latlng`, `date_scraped`) \
-      VALUES (%s, %s, %s, %s, %s, %s);", (scraper_id, item['item_id'], unique_hash, item['date'], item['latlng'],str_now))
+      VALUES (%s, %s, %s, %s, %s, %s);", (scraper_id, item['item_id'], unique_hash, item['date'], item['latlng'], str_now))
   
     for k,v in data.items():  
-      c.execute("""INSERT INTO `kv` (`item_id`,`key`,`value`) VALUES (%s, %s, %s);""", (item['item_id'], k,v))
+      c.execute("""INSERT INTO kv (`item_id`,`key`,`value`) VALUES (%s, %s, %s);""", (item['item_id'], k,v))
     
       # clean for printing to the console
 
+  ldata = { }
   for k,v in data.items():  
-    data[k] = cgi.escape(str(v))
-    
-    
+    ldata[k] = cgi.escape(str(v))
   
-  print '<scraperwiki:message type="data">%s' % json.dumps(data)
+  print '<scraperwiki:message type="data">%s' % json.dumps(ldata)
   return new_item_id
 
 
+
+def load(unique_keys, data):
+  """
+  Load function
+  """
   
+  return __load_row(unique_keys, data)
+
+
+def __load_row(unique_keys, data):
+  """
+  Loads a single row matching unique keys with same data
+  """
+  DUMMY_RUN = True
+  if os.environ.has_key('SCRAPER_GUID'):
+    scraper_id = os.environ['SCRAPER_GUID']
+    DUMMY_RUN = False
+  
+  # Create a unique hash
+  unique_hash = __create_unique(unique_keys, data)
+  
+  item = {'unique_hash' : unique_hash}
+  
+  new_item_id = None    
+  if not DUMMY_RUN:
+    conn = connection.Connection()
+    c = conn.connect()
+    
+    if not c.execute("SELECT scraper_id, item_id, unique_hash, date, latlng, date_scraped FROM items WHERE unique_hash=%s", (unique_hash,)):
+      return { }
+    lscraper_id, item_id, lunique_hash, date, latlng, date_scraped = c.fetchone()
+    
+    # this allows us to fetch entries from other scrapers.  
+    # we need some way to relate the SCRAPER_GUID to the scraper short_name
+    # assert lscraper_id == scraper_id
+    
+    rdata = { "date_scraped": date_scraped }  
+  
+    # why do the columns need to be in quotes?
+    c.execute("SELECT `key`, `value` FROM kv WHERE item_id=%s", (item_id,))
+    for key, value in c.fetchall():
+      rdata[key] = value
+    
+  return rdata
   
   
 if __name__ == "__main__":
