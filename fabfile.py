59632ab9008412a35f5384e2299ea52269d2ad00
Anna <anna@scraperwiki.com>
2010-01-11 15:23:38 +0000

    Updated fabfile.py to run with version 0.9 of fabric

--- fabfile.py
+++ fabfile.py
@@ -6,43 +6,44 @@ PROJECT_NAME = 'ScraperWiki'
 # environments
 def dev():
     "On the scrpaerwiki server, accessible from http://dev.scraperwiki.com"
-    config.fab_hosts = ['212.84.75.28']
-    config.path = '/var/www/dev.scraperwiki.com'
-    config.branch = 'default'
-    config.web_path = 'file:///home/scraperwiki/scraperwiki'
-    config.activate = config.path + '/bin/activate'
-    config.fab_user = 'scraperdeploy'
-    config.virtualhost_path = "/"
-    config.deploy_version = "Dev"
+    env.hosts = ['212.84.75.28']
+    env.path = '/var/www/dev.scraperwiki.com'
+    env.branch = 'default'
+    env.web_path = 'file:///home/scraperwiki/scraperwiki'
+    env.activate = env.path + '/bin/activate'
+    env.user = 'scraperdeploy'
+    env.virtualhost_path = "/"
+    env.deploy_version = "Dev"
 
 def alpha():
     "On the scrpaerwiki server, accessible from http://alpha.scraperwiki.com"
-    config.fab_hosts = ['212.84.75.28']
-    config.path = '/var/www/alpha.scraperwiki.com'
-    config.branch = 'stable'
-    config.web_path = 'file:///home/scraperwiki/scraperwiki'
-    config.activate = config.path + '/bin/activate'
-    config.fab_user = 'scraperdeploy'
-    config.virtualhost_path = "/"
-    config.deploy_version = "Alpha"
+    env.hosts = ['212.84.75.28']
+    env.path = '/var/www/alpha.scraperwiki.com'
+    env.branch = 'stable'
+    env.web_path = 'file:///home/scraperwiki/scraperwiki'
+    env.activate = env.path + '/bin/activate'
+    env.user = 'scraperdeploy'
+    env.virtualhost_path = "/"
+    env.deploy_version = "Alpha"
 
 def setup():
     """
     Setup a fresh virtualenv as well as a few useful directories, then run
     a full deployment
     """
-    require('path')
-    sudo('hg clone file:///home/scraperwiki/scraperwiki $(path)')        
-    sudo('chown -R %s %s' % (config.fab_user, config.path))
-    sudo('cd $(path); easy_install virtualenv')
-    run('hg clone $(web_path) $(path)', fail='ignore')
-    run('cd $(path); virtualenv --no-site-packages .')
+
+    sudo('hg clone file:///home/scraperwiki/scraperwiki %s' % env.path)        
+    sudo('chown -R %s %s' % (env.fab_user, env.path))
+    sudo('cd %s; easy_install virtualenv' % env.path)
+    run('hg clone %s %s', fail='ignore' % (env.web_path, env.path))
+    run('cd %s; virtualenv --no-site-packages .' % env.path)
     virtualenv('easy_install pip')
 
     deploy()
 
 def virtualenv(command):
-    run('cd $(path); source ' + config.activate + '&&' + command)
+    temp = 'cd %s; source ' % env.path
+    run(temp + env.activate + '&&' + command)
 
 
 def buildout():
@@ -62,11 +63,10 @@ def deploy():
     print "Please Enter your deploy message: \r"
     message = raw_input()
 
-    require('path','branch')
     import time
-    config.release = time.strftime('%Y%m%d%H%M%S')
+    env.release = time.strftime('%Y%m%d%H%M%S')
     
-    run('cd $(path); hg pull; hg update -C $(branch)')
+    run('cd %s; hg pull; hg update -C %s' % (env.path, env.branch))
     
     buildout()
     migrate()
