df99d149f169a62e4c45dbb338f98d0ee048a37b
goatchurch <goatchurch@ubuntu.clocksoft.dom>
2010-01-19 14:48:59 +0000

    make JSON be Orbited.JSON

--- web/media/js/editor.js
+++ web/media/js/editor.js
@@ -279,7 +279,7 @@ $(document).ready(function() {
     //send a message to the server
     function send(json_data) {
       conn.send(
-        JSON.stringify(json_data)
+        Orbited.JSON.stringify(json_data)
         );  
     }
 
