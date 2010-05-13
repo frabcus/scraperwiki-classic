221ab9c3c87db73d10d2f292ac95db7c0da86b54
Tom Mortimer-Jones <tom@morty.co.uk>
2010-05-13 16:53:34 +0100

    Small fix

--- uml/httpproxy/httpproxy.py
+++ uml/httpproxy/httpproxy.py
@@ -204,7 +204,7 @@ class HTTPProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler) :
 
     def blockmessage(self, url):
         qurl = urllib.quote(url)
-        return """Scraperwiki blocked access to "%s".  Click <a href="/whitelist/url=%s">here</a> for details.""" % (url, qurl)
+        return """Scraperwiki blocked access to "%s".  Click <a href="/whitelist/?url=%s">here</a> for details.""" % (url, qurl)
 
 
     def do_CONNECT (self) :
