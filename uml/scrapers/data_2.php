fb5d5202636d6aee56cd088ab9feaae9984f92c1
Mike Richardson <mike@quaking.demon.co.uk>
2010-06-16 13:01:05 +0100

    Add files

---
+++ uml/scrapers/data_2.php
@@ -0,0 +1,33 @@
+require_once  'scraperwiki/datastore.php'         ;
+print "GO\n" ;
+sw_data_save
+   (  array('key'),
+      array
+         (  'key'       => 'key_1',
+            'message'   => 'HELLO_1',
+            'arg1'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_1',
+            'arg2'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_2',
+            'arg3'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_3',
+            'arg4'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_4',
+            'arg5'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_5',
+            'arg6'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_6',
+            'arg7'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_7',
+            'arg8'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_8'
+         )
+   ) ;
+sw_data_save
+   (  array('key'),
+      array
+         (  'key'       => 'key_2',
+            'message'   => 'HELLO_2',
+            'arg1'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_1',
+            'arg2'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_2',
+            'arg3'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_3',
+            'arg4'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_4',
+            'arg5'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_5',
+            'arg6'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_6',
+            'arg7'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_7',
+            'arg8'      => 'ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_ARG_8'
+         )
+   ) ;
+print "OK\n" ;
