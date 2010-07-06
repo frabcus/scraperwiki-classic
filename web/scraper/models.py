0287f67894a5a60ab42109e959f8809b7e6a3718
Tom Mortimer-Jones <tom@morty.co.uk>
2010-07-06 14:22:10 +0100

    Comment out Ruby options

--- web/scraper/models.py
+++ web/scraper/models.py
@@ -30,7 +30,7 @@ from django.core.mail import send_mail
 LANGUAGES = (
     ('Python', 'Python'),
     ('PHP', 'PHP'),
-    ('Ruby', 'Ruby'),
+#    ('Ruby', 'Ruby'),
 )
 
 class Scraper(models.Model):
