0048e6a84202e05fe712376707dc2c50b46f0ea5
goatchurch <devnull@localhost>
2010-09-30 17:16:12 +0100

    implement the kill running runid for when Mike commits it

--- web/codewiki/urls.py
+++ web/codewiki/urls.py
@@ -30,7 +30,7 @@ urlpatterns = patterns('',
     # events and monitoring (pehaps should have both wiki_types possible)
     url(r'^scrapers/running_scrapers/$',                  viewsuml.running_scrapers,    name='running_scrapers'),
     url(r'^scrapers/run/(?P<event_id>[\w_\-]+)/$',        viewsuml.run_event,           name='run_event'),
-    url(r'^scrapers/scraper_killrunning/(?P<run_id>[\w_\-\.]+)/$',        
+    url(r'^scrapers/scraper_killrunning/(?P<run_id>[\w_\-\.]+)(?:/(?P<event_id>[\w_\-]+))?$',
                                                           viewsuml.scraper_killrunning, name='scraper_killrunning'),
         
     url(r'^scrapers/commit/(?P<event_id>\d+)/$',          views.commit_event,           name='commit_event'),
