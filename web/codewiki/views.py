b531f4ceb7e874682bbf54579422cd6977e0d9fb
goatchurch <devnull@localhost>
2010-10-14 03:53:08 +0100

    someone forgot to import render_to_string

--- web/codewiki/views.py
+++ web/codewiki/views.py
@@ -1,5 +1,6 @@
 from django.contrib.sites.models import Site
 from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
