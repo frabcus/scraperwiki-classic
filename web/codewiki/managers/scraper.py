e5bf3caeb5bf8fc6002d50b118fbf0f8718ffbc9
Mike Richardson <mike@quaking.demon.co.uk>
2010-12-21 15:41:29 +0000

    Access to datastore via dataproxy for clear_datastore and data_dictlist functionality. uml/dataproxy/datalib.py restructured as a class. Part of scraperlibs/scraperwiki/datastore/datastore.py moved to web/codewiki/managers/scraper.py. Added DATAPROXY_HOST and DATAPROXY_PORT into settings.py

--- web/codewiki/managers/scraper.py
+++ web/codewiki/managers/scraper.py
@@ -7,6 +7,7 @@ import re
 import datetime
 import types
 from code import CodeManager
+from datastore import  DataStore
 
 def convert_dictlist_to_datalist(allitems, column_order=None, private_columns=None):
 
@@ -53,15 +54,14 @@ class ScraperManager(CodeManager):
             'DATABASE_TIME_ZONE': settings.TIME_ZONE,
         })
         super(ScraperManager, self).__init__(*args, **kwargs)
-    
 
     def get_query_set(self):
         return super(ScraperManager, self).get_query_set().filter(deleted=False)
         
-        	
+            
     def owns(self):
         return self.get_query_set().filter(usercoderole__role='owner')
-		
+        
     def watching(self):
         return self.get_query_set().filter(usercoderole__role='follow')
 
@@ -78,7 +78,7 @@ class ScraperManager(CodeManager):
         return len(self.owns())
         
     def owned_good_count(self):
-        return len(self.owned_good())	
+        return len(self.owned_good())   
         
     def watching_count(self):
         return len(self.watching())
@@ -89,13 +89,18 @@ class ScraperManager(CodeManager):
     def dont_own_any(self):
         return self.owned_count() == 0
 
+    def dataproxy(self, scraper_id):
+        dataproxy = DataStore(scraper_id)
+        return dataproxy
+
     def clear_datastore(self, scraper_id):
-        c = self.datastore_connection.cursor()
-        c.execute("delete kv items from kv inner join items where items.item_id = kv.item_id and items.scraper_id=%s", (scraper_id,))
-        c.close()
-        self.datastore_connection.connection.commit()
+        proxy   = self.dataproxy(scraper_id)
+        rc, arg = proxy.clear_datastore()
+        if not rc :
+            raise Exception(arg)
 
     def datastore_keys(self, scraper_id):
+
         result = []
         c = self.datastore_connection.cursor()
         c.execute("select distinct kv.key from items inner join kv on kv.item_id=items.item_id WHERE items.scraper_id=%s", (scraper_id,))        
@@ -107,6 +112,7 @@ class ScraperManager(CodeManager):
         return result
 
     def data_search(self, scraper_id, key_values, limit=1000, offset=0):   
+
         qquery = ["SELECT items.item_id, COUNT(items.item_id) as item_count"]
         qlist  = [ ]
 
@@ -180,104 +186,13 @@ class ScraperManager(CodeManager):
     def data_dictlist(self, scraper_id, limit=1000, offset=0, start_date=None, end_date=None, latlng=None):   
         '''map from scraper_id and filters to dict representing row record for a particular scraper'''
 
-        # previously implemented with a sub-select table joined on kv.  
-        # Now implemented by fetching the items, and building each row separately
-        
-        # split the latlng into a pair
-        # code here similar to datastore/save.py __build_matches
-
-        qquery = ["SELECT items.item_id AS item_id"]
-        qlist  = [ ]
-
-        if latlng:
-            #qquery.append(", SUBSTR(items.latlng, 1, 20)")
-            #qquery.append(", SUBSTR(items.latlng, 21, 41)")
-            #qquery.append(", ABS(SUBSTR(items.latlng, 1, 20)-%s)+ABS(SUBSTR(items.latlng, 21, 41)-%s) AS diamdist")
-            qquery.append(", ((ACOS(SIN(%s * PI() / 180) * SIN(ABS(SUBSTR(items.latlng, 1, 20)) * PI() / 180) + COS(%s * PI() / 180) * COS(ABS(SUBSTR(items.latlng, 1, 20)) * PI() / 180) * COS((%s - ABS(SUBSTR(items.latlng, 21, 41))) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) AS distance")
-            qlist.append(latlng[0])
-            qlist.append(latlng[0])
-            qlist.append(latlng[1])                        
-
-            #qlist.append(latlng[0])
-            #qlist.append(latlng[1])
-            #qquery.append(", items.latlng AS latlng")
-        
-        qquery.append("FROM items")
-
-        # add the where clause
-        qquery.append("WHERE items.scraper_id=%s")
-        qlist.append(scraper_id)
-        
-        # filter by latlng exists; (can't filter by distance with this object!)
-        #if latlng:
-        #    qquery.append("AND items.latlng IS NOT NULL")
-        
-        if start_date and end_date:
-            #qquery.append("AND items.`date` IS NOT NULL")
-            qquery.append("AND items.`date` >= %s")
-            qlist.append(start_date)
-            qquery.append("AND items.`date` < %s")
-            qlist.append(end_date)
-
-        if latlng:
-            qquery.append("AND not isnull(items.latlng)")            
-            qquery.append("HAVING distance < %s")
-            qlist.append(settings.MAX_API_DISTANCE_KM)            
-            qquery.append("ORDER BY distance ASC")
-        else:
-            qquery.append("ORDER BY date_scraped DESC")
-
-        qquery.append("LIMIT %s,%s")
-        qlist.append(offset)
-        qlist.append(limit)
-
-        #print " ".join(qquery) %tuple(qlist)
-        c = self.datastore_connection.cursor()
-
-        c.execute(" ".join(qquery), tuple(qlist))
-        item_idlist = c.fetchall()
-
-        # code here similar to datastore/save.py __retrieve_item
-
-        allitems = [ ]
-        for item_idl in item_idlist:
-            
-            #get the item ID and create an object for the data to live in
-            item_id = item_idl[0]
-            rdata = { }
-
-            #add distance if present
-            if len(item_idl) > 1:
-                rdata['distance'] = item_idl[1]
-
-            # header records
-            if not c.execute("SELECT `date`, latlng, `date_scraped` FROM items WHERE item_id=%s", (item_id,)):
-                continue  #TODO: raise an exception 
-            item = c.fetchone()
-            
-            if item[0]:
-                rdata["date"] = item[0]           
-            if item[2]:
-                rdata["date_scraped"] = item[2]
-
-            # put the key values in
-            c.execute("SELECT `key`, `value` FROM kv WHERE item_id=%s", (item_id,))
-            for key, value in c.fetchall():
-                rdata[key] = value
-
-            # over-ride any values with latlng (we could break it into two values) (may need to wrap in a try to protect)
-            if item[1]:
-                rdata["latlng"] = tuple(map(float, item[1].split(",")))
-            else:
-                rdata.pop("latlng", None)  # make sure this field is always a pair of floats
-        
-            allitems.append(rdata)
-
-        c.close()
+        proxy   = self.dataproxy(scraper_id)
+        rc, arg = proxy.data_dictlist(limit, offset, start_date, end_date, latlng)
+        if not rc :
+            raise Exception(arg)
+        allitems = arg
         return allitems
-           
-              
-    
+
     def data_summary(self, scraper_id=0, limit=1000, offset=0, start_date=None, end_date=None, latlng=None, column_order=None, private_columns=None):
         '''single table of all rows for a scraper'''
         allitems = self.data_dictlist(scraper_id, limit=limit, offset=offset, start_date=start_date, end_date=start_date, latlng=latlng)  
