180d927b8c7c4ae34e1e2efaef1bcbb6d580841a
rossjones <devnull@localhost>
2011-01-20 11:29:24 +0000

    Updated the registration tests to include a successful registration

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, uuid
 from copy import deepcopy
 from selenium import selenium
 from selenium_test import SeleniumTest
@@ -13,6 +13,29 @@ class TestRegistration(SeleniumTest):
         "id_password1": "password",                
         "id_password2": "password",                               
     }
+    
+
+    def test_create_valid(self):
+        s = self.selenium
+        s.open("/")
+        s.click("link=Sign in or create an account")
+        s.wait_for_page_to_load("30000")
+
+        username = str( uuid.uuid4() ).replace('-', '_')
+
+        d = deepcopy( self.default_values )
+        d["id_username"] = "test_%s" % (username,)                               
+        print 'Username is %s' % (d["id_username"],)
+        
+        self.type_dictionary( d )
+        s.click( 'id_tos' )
+        
+        s.click('register')
+        s.wait_for_page_to_load("30000")
+        
+        self.failUnless(s.is_text_present("signed in as"))
+
+
 
     def test_nonmatching_passwords(self):
         s = self.selenium
