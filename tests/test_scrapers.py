4b11d6f32a0f89fba3b5d65285f74b6d631f23ce
rossjones <devnull@localhost>
2011-01-20 17:08:00 +0000

    Set the three basic scrapers to include the code from the starter templates, and allowed the scraper tests to load them from disk and save them in the database

--- tests/test_scrapers.py
+++ tests/test_scrapers.py
@@ -1,4 +1,5 @@
-import unittest, uuid, time
+import os, sys, unittest, uuid, time
+
 from copy import deepcopy
 from selenium import selenium
 from selenium_test import SeleniumTest
@@ -6,9 +7,33 @@ from selenium_test import SeleniumTest
 
 class TestScrapers(SeleniumTest):
     
-    def test_create(self):
+    def load_scraper(self, type='python'):
+        thefile = os.path.join( os.path.dirname( __file__ ), 'sample_data/%s_scraper.txt' % type)
+        try:
+            f = open(thefile)
+            code = f.read()
+            f.close()
+        except:
+            code = ''
+    
+        return code
+        
+    
+    def test_python_create(self):
+        self.create_type( 'Blank Python scraper', 'python')
+        # TODO: Click on data tab and see what is there.
+        
+    def test_ruby_create(self):        
+        self.create_type( 'Blank Ruby scraper', 'ruby')
+        # TODO: Click on data tab and see what is there.
+                
+    def test_php_create(self):        
+        self.create_type( 'Blank PHP scraper', 'php')                
+        # TODO: Click on data tab and see what is there.    
+    
+    def create_type(self, link_name, type):
         name = str( uuid.uuid4() )
-        print 'Scraper will be called ', name
+        print '%s scraper will be called %s' % (type, name,)
         
         s = self.selenium
         s.open("/logout")
@@ -17,9 +42,11 @@ class TestScrapers(SeleniumTest):
         s.answer_on_next_prompt( name )        
         s.click('link=New scraper')        
         time.sleep(1)        
-        s.click( 'link=Blank Python scraper' )
+        s.click( 'link=%s' % link_name )
         s.wait_for_page_to_load("30000")      
-        s.type('//body[@class="editbox"]', "print 'hello'")        
+        
+        code = self.load_scraper(type)
+        s.type('//body[@class="editbox"]', "%s" % code)        
         s.click('btnCommitPopup')
         s.wait_for_page_to_load("30000")      
         time.sleep(1)
