7991ce46cab18016085b0f95f97d7d1261e9438d
Tom Mortimer-Jones <tom@morty.co.uk>
2011-02-04 14:32:15 +0000

    Fix for month_name filter.

--- web/frontend/templatetags/month_name.py
+++ web/frontend/templatetags/month_name.py
@@ -3,10 +3,11 @@ import calendar
 
 register = Library()
 
-@register.simple_tag
 def month_name(number):
     try:
         num = int(number)
         return calendar.month_name[num]
     except:
         return ""
+
+register.filter('month_name', month_name)
