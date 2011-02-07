711c30700737c2a0ef0bf88fa7e800e3d0e8db7c
goatchurch <devnull@localhost>
2011-02-07 10:58:16 +0000

    convenient url run redirect

--- web/codewiki/urls.py
+++ web/codewiki/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.defaults import *
 from piston.resource import Resource
 
 from handlers import ScraperMetadataHandler
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 
 metadata = Resource(handler=ScraperMetadataHandler)
 
@@ -14,12 +17,16 @@ urlpatterns = patterns('',
     # use this to monitor the site
     url(r'^table/$',                                      views.scraper_table,          name='scraper_table'),
 
-    # special views functionality
+    # running a scraper by calling a url
     url(r'^run/(?P<short_name>[\w_\-\.]+)/(?P<revision>\d+/)?$', 
                                                           viewsrpc.rpcexecute,          name='rpcexecute'),    
-    url(r'^views/(?P<short_name>[\w_\-\.]+)/html/$',        views.htmlview,               name='htmlview'),
+    url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/run/$',   # redirect because it's so common
+                   lambda request, wiki_type, short_name: HttpResponseRedirect(reverse('rpcexecute', args=[short_name]))),
+    
+    # special views functionality
+    url(r'^views/(?P<short_name>[\w_\-\.]+)/html/$',      views.htmlview,               name='htmlview'),
     url(r'^views/(?P<short_name>[\w_\-\.]+)/full/$',      views.view_fullscreen,        name='view_fullscreen'),   
-    url(r'^views/(?P<short_name>[\w_\-\.]+)/admin/$',       views.view_admin,             name='view_admin'),    
+    url(r'^views/(?P<short_name>[\w_\-\.]+)/admin/$',     views.view_admin,             name='view_admin'),    
     
     url(r'^scrapers/delete-data/(?P<short_name>[\w_\-\.]+)/$', views.scraper_delete_data, name='scraper_delete_data'),
     url(r'^scrapers/export/(?P<short_name>[\w_\-\.]+)/$', views.export_csv,             name='export_csv'),
@@ -27,8 +34,8 @@ urlpatterns = patterns('',
                                                           views.export_sqlite,          name='export_sqlite'),
     url(r'^scrapers/export2/(?P<short_name>[\w_\-\.]+)/$',views.export_gdocs_spreadsheet,name='export_gdocs_spreadsheet'),    
     
-    url(r'^scrapers/follow/(?P<short_name>[\w_\-\.]+)/$',   views.follow,                 name='scraper_follow'),
-    url(r'^scrapers/unfollow/(?P<short_name>[\w_\-\.]+)/$', views.unfollow,               name='scraper_unfollow'),
+    url(r'^scrapers/follow/(?P<short_name>[\w_\-\.]+)/$',   views.follow,               name='scraper_follow'),
+    url(r'^scrapers/unfollow/(?P<short_name>[\w_\-\.]+)/$', views.unfollow,             name='scraper_unfollow'),
     
     url(r'^scrapers/metadata_api/(?P<scraper_guid>[\w_\-\.]+)/(?P<metadata_name>.+)/$', metadata, name='metadata_api'),
 
@@ -50,7 +57,7 @@ urlpatterns = patterns('',
                                                           views.scraper_screenshoot_scraper,    name='scraper_screenshoot_scraper'),
         
         #not deprecated as used in by ajax to implement publishScraperButton
-    url(r'^scrapers/(?P<short_name>[\w_\-\.]+)/admin/$',    views.scraper_admin,          name='scraper_admin'),
+    url(r'^scrapers/(?P<short_name>[\w_\-\.]+)/admin/$',    views.scraper_admin,        name='scraper_admin'),
         
     url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/$',          views.code_overview,    name='code_overview'),
     url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/history/$',  views.scraper_history,  name='scraper_history'),
@@ -58,7 +65,7 @@ urlpatterns = patterns('',
     url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/code/$',     viewseditor.code,       name='scraper_code'),    
     url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/tags/$',     views.tags,             name='scraper_tags'),    
         
-    url(r'^(?P<wiki_type>scraper|view)s/new/choose_template/$',               views.choose_template, name='choose_template'),    
+    url(r'^(?P<wiki_type>scraper|view)s/new/choose_template/$',                 views.choose_template,  name='choose_template'),    
     url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/raw_about_markup/$', views.raw_about_markup, name='raw_about_markup'),        
     
         # redirects to new scraper?template=
@@ -74,7 +81,7 @@ urlpatterns = patterns('',
     # editor 
     url(r'^handle_session_draft/$',                       viewseditor.handle_session_draft, name="handle_session_draft"),
     url(r'^handle_editor_save/$',                         viewseditor.handle_editor_save,   name="handle_editor_save"),    
-    url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/edit/$',     viewseditor.edit, name="editor_edit"),    
+    url(r'^(?P<wiki_type>scraper|view)s/(?P<short_name>[\w_\-\.]+)/edit/$',   viewseditor.edit, name="editor_edit"),    
     url(r'^(?P<wiki_type>scraper|view)s/new/(?P<language>[\w]+)$',            viewseditor.edit, name="editor"),
 )
 
