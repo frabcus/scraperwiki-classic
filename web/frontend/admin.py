6d7cb035ec2fa9643a40b2d0c09c712302f950d2
Francis Irving <francis@scraperwiki.com>
2011-02-25 11:54:02 +0000

    Show active, date joined, last login on top level user admin page

--- web/frontend/admin.py
+++ web/frontend/admin.py
@@ -1,5 +1,6 @@
 from frontend.models import *
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Don't put AlertTypes in admin, as we need to control them via fixtures only
 class AlertTypesAdmin(admin.ModelAdmin):
@@ -16,8 +17,17 @@ class AlertsAdmin(admin.ModelAdmin):
 class MessageAdmin(admin.ModelAdmin):
     pass
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined', 'last_login')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+
+
 admin.site.register(UserProfile)
 admin.site.register(UserToUserRole)
 admin.site.register(AlertTypes, AlertTypesAdmin)
 admin.site.register(Alerts, AlertsAdmin)
 admin.site.register(Message, MessageAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
