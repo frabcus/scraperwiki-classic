55e4a0006ffdf24c4c35989e086c84d2f16c554d
Tom Mortimer-Jones <tom@morty.co.uk>
2011-03-01 10:59:27 +0000

    Fix for unicode problem on history page

--- web/codewiki/views.py
+++ web/codewiki/views.py
@@ -396,7 +396,7 @@ def scraper_history(request, wiki_type, short_name):
                 runduration = runevent.run_ended - runevent.run_started
                 item["runduration"] = runduration
                 item["durationseconds"] = "%.0f" % (runduration.days*24*60*60 + runduration.seconds)
-            item["groupkey"] = "runevent|||"+ str(runevent.exception_message)
+            item["groupkey"] = "runevent|||"+ str(runevent.exception_message.encode('utf-8'))
             itemlog.append(item)
         
         itemlog.sort(key=lambda x: x["datetime"], reverse=True)
