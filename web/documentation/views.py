59abf4ceb9bf101d79df88e40fa16817a669cf70
Tom Mortimer-Jones <tom@morty.co.uk>
2011-04-27 14:58:47 +0100

    Raise 404 error if documentation template is not found.

--- web/documentation/views.py
+++ web/documentation/views.py
@@ -1,5 +1,6 @@
-from django.template import RequestContext
+from django.template import RequestContext, TemplateDoesNotExist
 from django.shortcuts import render_to_response
+from django.http import Http404
 
 def catchall(request, path=None):
     lang = request.GET.get('lang', None) or request.session.get('lang', 'python')
@@ -10,4 +11,7 @@ def catchall(request, path=None):
     else:
         template = 'documentation/index.html'
 
-    return render_to_response(template, {'lang': lang}, context_instance=RequestContext(request))
+    try:
+        return render_to_response(template, {'lang': lang}, context_instance=RequestContext(request))
+    except TemplateDoesNotExist:
+        raise Http404
