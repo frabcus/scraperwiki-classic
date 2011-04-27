9668e7116c0c012850bc7f50364e3602d0912e39
goatchurch <devnull@localhost>
2011-04-27 15:10:30 +0100

    documentation link from front page, fix mismatch tags and remove excess submenu

--- web/documentation/urls.py
+++ web/documentation/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 from documentation import views
 
 urlpatterns = patterns('',
-   url(r'^$', views.catchall, name="documentation"),
-   url(r'^(?P<path>.+)/$', views.catchall),
+   url(r'^$', views.catchall, name="docsroot"),
+#   url(r'^contrib/(?P<path>.+)/$', views.contribs, name="contrib"),
+   url(r'^(?P<path>.+)/$', views.catchall, name="docs"),
 )
