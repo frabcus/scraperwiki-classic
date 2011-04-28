f43f43a266182830f3d662ab632cd5f824263669
rossjones <devnull@localhost>
2011-04-28 18:10:04 +0100

    Added titles for each page in the dpocumentation

---
+++ web/documentation/titles.py
@@ -0,0 +1,37 @@
+
+# List of titles (and optional paragraph) for each of the documentation pages
+page_titles = {
+    "ajax_topic_guide": ("Using Ajax from ScraperWiki", "Para"),
+    "docindex" : ("",""),
+    "gmap_topic_guide" : ("",""),
+    "jquery_topic_guide" : ("",""),
+    
+    "php_datastore_guide" : ("The Datastore","A guide to the datastore for PHP programmers"),
+    "php_help_documentation" : ("ScraperWiki internal API",""),
+    "php_intro_tutorial" : ("Introductory tutorial",""),
+    "php_libraries" : ("PHP Libraries",""),
+    "php_view_guide" : ("ScraperWiki View Guide","How to use Views inside ScraperWiki"),    
+    
+    "python_datastore_guide" : ("The Datastore","A guide to the datastore for Python programmers"),
+    "python_help_documentation" : ("ScraperWiki internal API",""),
+    "python_intro_tutorial" : ("Introductory tutorial",""),
+    "python_libraries" : ("Python Libraries",""),
+    "python_view_guide" : ("ScraperWiki View Guide","How to use Views inside ScraperWiki"),    
+    
+    "ruby_datastore_guide" : ("The Datastore","A guide to the datastore for Ruby programmers"),
+    "ruby_help_documentation" : ("ScraperWiki internal API",""),
+    "ruby_intro_tutorial" : ("Introductory tutorial",""),
+    "ruby_libraries" : ("Ruby Libraries",""),
+    "ruby_view_guide" : ("ScraperWiki View Guide","How to use Views inside ScraperWiki"),    
+    
+    "view_guide" : ("ScraperWiki View Guide","How to use Views inside ScraperWiki"),    
+}
+
+
+scraper_titles = {
+    "excel_cheat_sheet" : ("Reading Excel files","How to read Excel files with ScraperWiki"),    
+    "python_lxml_cheat_sheet" : ("Using lxml",""),
+    "python_mechanize_cheat_sheet": ("Using Mechanize with Python", ""),
+    "csv_cheat_sheet": ("Working with CSV files", ""),
+    "python_querystring_cheat_sheet": ("Reading querystring parameters",""),
+}
