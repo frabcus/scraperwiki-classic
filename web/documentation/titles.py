da84fea8045db31422b2d6d049c1ec578e535f9f
francis <francis@flourish.org>
2011-04-30 19:40:00 +0100

    Improve wording.

--- web/documentation/titles.py
+++ web/documentation/titles.py
@@ -9,19 +9,19 @@ page_titles = {
     "faq" : ("Frequently Asked Questions",""),    
     
     "ruby_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already code Ruby"),
-    "ruby_datastore_guide" : ("Datastore guide","A guide to the datastore for Ruby programmers"),
+    "ruby_datastore_guide" : ("Datastore guide","A guide to the ScraperWiki SQL datastore for Ruby programmers"),
     "ruby_help_documentation" : ("ScraperWiki internal API",""),
     "ruby_libraries" : ("3rd party libraries",""),
     "ruby_view_guide" : ("ScraperWiki view guide","How to use Views inside ScraperWiki"),    
    
     "python_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already program in Python"),
-    "python_datastore_guide" : ("Datastore guide","A guide to the datastore for Python programmers"),
+    "python_datastore_guide" : ("Datastore guide","A guide to the ScraperWiki SQL datastore for Python programmers"),
     "python_help_documentation" : ("ScraperWiki internal API",""),
     "python_libraries" : ("3rd party libraries",""),
     "python_view_guide" : ("ScraperWiki View Guide","How to use Views inside ScraperWiki"),    
     
     "php_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already write PHP"),
-    "php_datastore_guide" : ("Datastore guide","A guide to the datastore for PHP programmers"),
+    "php_datastore_guide" : ("Datastore guide","A guide to the ScraperWiki SQL datastore for PHP programmers"),
     "php_help_documentation" : ("ScraperWiki internal API",""),
     "php_libraries" : ("3rd party libraries",""),
     "php_view_guide" : ("ScraperWiki view guide","How to use Views inside ScraperWiki"),    
