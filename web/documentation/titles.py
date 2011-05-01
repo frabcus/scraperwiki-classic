47e79183eececc550b9c21ad876ae3122818f9e4
francis <francis@flourish.org>
2011-05-01 11:22:53 +0100

    Update Ruby libraries docs to new formatting.

--- web/documentation/titles.py
+++ web/documentation/titles.py
@@ -11,18 +11,18 @@ page_titles = {
     "ruby_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already code Ruby"),
     "ruby_datastore_guide" : ("Datastore guide","A guide to the ScraperWiki SQL datastore for Ruby programmers"),
     "ruby_help_documentation" : ("ScraperWiki internal API",""),
-    "ruby_libraries" : ("3rd party libraries",""),
+    "ruby_libraries" : ("3rd party libraries","Ruby batteries included in ScraperWiki"),
     "ruby_view_guide" : ("ScraperWiki view guide","How to use Views inside ScraperWiki"),    
    
     "python_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already program in Python"),
     "python_datastore_guide" : ("Datastore guide","A guide to the ScraperWiki SQL datastore for Python programmers"),
     "python_help_documentation" : ("ScraperWiki internal API",""),
-    "python_libraries" : ("3rd party libraries",""),
+    "python_libraries" : ("3rd party libraries","Python batteries included in ScraperWiki"),
     "python_view_guide" : ("ScraperWiki View Guide","How to use Views inside ScraperWiki"),    
     
     "php_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already write PHP"),
     "php_datastore_guide" : ("Datastore guide","A guide to the ScraperWiki SQL datastore for PHP programmers"),
-    "php_help_documentation" : ("ScraperWiki internal API",""),
+    "php_help_documentation" : ("ScraperWiki internal API","PHP batteries included in ScraperWiki"),
     "php_libraries" : ("3rd party libraries",""),
     "php_view_guide" : ("ScraperWiki view guide","How to use Views inside ScraperWiki"),    
     
