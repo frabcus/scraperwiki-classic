ed019ded0a5358bcafd3677b8f3f76283937a740
francis <francis@flourish.org>
2011-05-01 14:07:52 +0100

    Add language in title

--- web/documentation/titles.py
+++ web/documentation/titles.py
@@ -12,19 +12,19 @@ page_titles = {
     "ruby_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for Ruby programmers"),
     "ruby_help_documentation" : ("Internal API","All the functions in the Ruby ScraperWiki module"),
     "ruby_libraries" : ("3rd party libraries","Ruby batteries included in ScraperWiki"),
-    "ruby_view_guide" : ("Views tutorial","How to use ScraperWiki views"),    
+    "ruby_view_guide" : ("Views tutorial","How to use ScraperWiki views in Ruby"),
    
     "python_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already program in Python"),
     "python_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for Python programmers"),
     "python_help_documentation" : ("Internal API","All the functions in the Python scraperwiki module"),
     "python_libraries" : ("3rd party libraries","Python batteries included in ScraperWiki"),
-    "python_view_guide" : ("Views tutorial","How to use ScraperWiki views"),    
+    "python_view_guide" : ("Views tutorial","How to use ScraperWiki views in Python"),
     
     "php_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already write PHP"),
     "php_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for PHP programmers"),
     "php_help_documentation" : ("Internal API","All the functions in the PHP scraperwiki namespace"),
     "php_libraries" : ("3rd party libraries","PHP batteries included in ScraperWiki"),
-    "php_view_guide" : ("Views tutorial","How to use ScraperWiki views"),    
+    "php_view_guide" : ("Views tutorial","How to use ScraperWiki views in PHP"),
     
     "technical_overview" : ("Technical overview","All the component parts of ScraperWiki and how they work together"),    
 }
