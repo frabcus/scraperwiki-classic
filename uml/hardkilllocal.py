4c6d73c3d58233fa0104580036b3f67ceac40c5b
francis <francis@flourish.org>
2011-05-06 20:29:19 +0100

    Make it compatible with OSX's ps (still works on Ubuntu I think)

--- uml/hardkilllocal.py
+++ uml/hardkilllocal.py
@@ -6,7 +6,7 @@
 
 import os
 for t in ['twister', 'www/scraperwiki/uml']:
-    for s in os.popen('ps -AF | grep %s' % t).readlines():
+    for s in os.popen('ps -Af | grep %s' % t).readlines():
         try:
             print "killing", s
             os.kill(int(s.split()[1]), 9)
