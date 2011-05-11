b12e639852ac913bda474605faec5dd0162d3ff8
goatchurch <devnull@localhost>
2011-05-11 15:08:04 +0100

    config parser technology into dataproxy

--- uml/dataproxy/dataproxy.py
+++ uml/dataproxy/dataproxy.py
@@ -1,55 +1,52 @@
 ##!/bin/sh -
 "exec" "python" "-O" "$0" "$@"
 
-__doc__ = """ScraperWiki Data Proxy"""
-
-__version__ = "ScraperWiki_0.0.1"
-
 import BaseHTTPServer
 import SocketServer
 import urllib
 import urlparse
 import cgi
-import select
 import signal
 import os
 import sys
 import time
-import threading
-import string 
-import hashlib
-import datalib
 import ConfigParser
 import datetime
+import optparse
 
 try   : import json
 except: import simplejson as json
 
-global config
+import datalib
 
 USAGE      = " [--varDir=dir] [--subproc] [--daemon] [--config=file]"
 child      = None
-config     = None
-varDir     = '/var'
-uid        = None
-gid        = None
 
-class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler) :
+parser = optparse.OptionParser()
+parser.add_option("--varDir", metavar="dir", default='/var')
+parser.add_option("--subproc", action="store_true")
+parser.add_option("--daemon", action="store_true")
+parser.add_option("--uid")
+parser.add_option("--gid")
+parser.add_option("--config", dest="confnam", metavar="file", default='uml.cfg')
+poptions, pargs = parser.parse_args()
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(poptions.confnam))
+
+
+class ProxyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     __base         = BaseHTTPServer.BaseHTTPRequestHandler
     __base_handle  = __base.handle
 
-    server_version = "DataProxy/" + __version__
+    server_version = "DataProxy/ScraperWiki_0.0.1"
     rbufsize       = 0
 
-    def __init__ (self, *alist, **adict) :
-        self.m_db      = None
-        BaseHTTPServer.BaseHTTPRequestHandler.__init__ (self, *alist, **adict)
-
     def log_message (self, format, *args) :
-        BaseHTTPServer.BaseHTTPRequestHandler.log_message (self, format, *args)
-        sys.stderr.flush ()
+        BaseHTTPServer.BaseHTTPRequestHandler.log_message(self, format, *args)
+        sys.stderr.flush()
 
-    def ident (self, uml, port) :
+    def ident(self, uml, port):
         scraperID   = None
         runID       = None
         scraperName = None
@@ -60,16 +57,16 @@ class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler) :
         #  supplied in the request since the peer port will have been subject to
         #  NAT or masquerading.
         #
-        host      = config.get (uml, 'host')
-        via       = config.get (uml, 'via' )
+        host      = config.get(uml, 'host')
+        via       = config.get(uml, 'via' )
         rem       = self.connection.getpeername()
         loc       = self.connection.getsockname()
-        ident     = urllib.urlopen ('http://%s:%s/Ident?%s:%s' % (host, via, port, loc[1])).read()   # (lucky this doesn't clash with the function we are in, eh -- JT)
+        lident     = urllib.urlopen ('http://%s:%s/Ident?%s:%s' % (host, via, port, loc[1])).read()   
 
                 # should be using cgi.parse_qs(query) technology here
-        for line in string.split (ident, '\n') :
+        for line in lident.split('\n'):
             if line:
-                key, value = string.split (line, '=')
+                key, value = line.split('=')
                 if key == 'runid':
                     runID = value
                 elif key == 'scraperid':
@@ -155,88 +152,34 @@ class ProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler) :
     do_DELETE = do_GET
 
 
-class ProxyHTTPServer \
-        (   SocketServer.ForkingMixIn,
-            BaseHTTPServer.HTTPServer
-        ) :
+class ProxyHTTPServer(SocketServer.ForkingMixIn, BaseHTTPServer.HTTPServer):
     pass
 
 
-def execute (port) :
-
-    ProxyHandler.protocol_version = "HTTP/1.0"
-
-    httpd = ProxyHTTPServer(('', port), ProxyHandler)
-    sa    = httpd.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
-
-    httpd.serve_forever()
-
-
-def sigTerm (signum, frame) :
-
-    try    : os.kill (child, signal.SIGTERM)
+def sigTerm(signum, frame) :
+    try    : os.kill(child, signal.SIGTERM)
     except : pass
-    try    : os.remove (varDir + '/run/dataproxy.pid')
+    try    : os.remove(poptions.varDir + '/run/dataproxy.pid')
     except : pass
     sys.exit (1)
 
 
 if __name__ == '__main__' :
 
-    subproc = False
-    daemon  = False
-    confnam = 'uml.cfg'
-
-    for arg in sys.argv[1:] :
-
-        if arg in ('-h', '--help') :
-            print "usage: " + sys.argv[0] + USAGE
-            sys.exit (1)
-
-        if arg[: 6] == '--uid=' :
-            uid      = arg[ 6:]
-            continue
-
-        if arg[: 6] == '--gid=' :
-            gid      = arg[ 6:]
-            continue
-
-        if arg[ :9] == '--varDir='  :
-            varDir  = arg[ 9:]
-            continue
-
-        if arg[ :9] == '--config='  :
-            confnam = arg[ 9:]
-            continue
-
-        if arg == '--subproc' :
-            subproc = True
-            continue
-
-        if arg == '--daemon' :
-            daemon = True
-            continue
-
-        print "usage: " + sys.argv[0] + USAGE
-        sys.exit (1)
-
-
     #  If executing in daemon mode then fork and detatch from the
     #  controlling terminal. Basically this is the fork-setsid-fork
     #  sequence.
     #
-    if daemon :
-
+    if poptions.daemon :
         if os.fork() == 0 :
             os .setsid()
             sys.stdin  = open ('/dev/null')
-            sys.stdout = open (varDir + '/log/dataproxy', 'a', 0)
+            sys.stdout = open (poptions.varDir + '/log/dataproxy', 'a', 0)
             sys.stderr = sys.stdout
             if os.fork() == 0 :
                 ppid = os.getppid()
                 while ppid != 1 :
-                    time.sleep (1)
+                    time.sleep(1)
                     ppid = os.getppid()
             else :
                 os._exit (0)
@@ -244,34 +187,33 @@ if __name__ == '__main__' :
             os.wait()
             sys.exit (1)
 
-        pf = open (varDir + '/run/dataproxy.pid', 'w')
+        pf = open (poptions.varDir + '/run/dataproxy.pid', 'w')
         pf.write  ('%d\n' % os.getpid())
         pf.close  ()
 
-    if gid is not None : os.setregid (int(gid), int(gid))
-    if uid is not None : os.setreuid (int(uid), int(uid))
+    if poptions.gid is not None:
+        os.setregid(int(poptions.gid), int(poptions.gid))
+    if poptions.uid is not None:
+        os.setreuid(int(poptions.uid), int(poptions.uid))
 
     #  If running in subproc mode then the server executes as a child
     #  process. The parent simply loops on the death of the child and
     #  recreates it in the event that it croaks.
-    #
-    if subproc :
-
-        signal.signal (signal.SIGTERM, sigTerm)
-
-        while True :
-
+    if poptions.subproc:
+        signal.signal(signal.SIGTERM, sigTerm)
+        while True:
             child = os.fork()
-            if child == 0 :
+            if child == 0:
                 break
 
             sys.stdout.write("%s: Forked subprocess: %d\n" % (datetime.datetime.now().ctime(), child))
             sys.stdout.flush()
-    
             os.wait()
 
 
-    config = ConfigParser.ConfigParser()
-    config.readfp (open(confnam))
-
-    execute (config.getint ('dataproxy', 'port'))
+    port = config.getint('dataproxy', 'port')
+    ProxyHandler.protocol_version = "HTTP/1.0"
+    httpd = ProxyHTTPServer(('', port), ProxyHandler)
+    sa = httpd.socket.getsockname()
+    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    httpd.serve_forever()
