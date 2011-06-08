43df282520b45f1b868305c038df2ce6d27a67e1
francis <francis@flourish.org>
2011-06-08 14:40:20 +0100

    Make update_kpi work for record count

--- web/kpi/management/commands/update_kpis.py
+++ web/kpi/management/commands/update_kpis.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
-from kpi.models import DatastoreRecordCount
+from django.db.models import Sum
+
+from kpi.models import DatastoreRecordCount, MonthlyCounts
+from codewiki.models import Scraper, Code
+
 import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        DatastoreRecordCount.objects.create(date=datetime.date.today(),
-                                            record_count=0)
+        # count records in database
+        record_count_now = Scraper.objects.aggregate(record_count=Sum('record_count'))['record_count']
+        DatastoreRecordCount.objects.create(date=datetime.date.today(), record_count=record_count_now)
