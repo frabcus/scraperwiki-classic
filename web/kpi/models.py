63a65315d2f037bf06ffa7df9f32e2c6de445413
francis <francis@flourish.org>
2011-06-08 14:32:37 +0100

    Migration for storing monthly KPI values.

--- web/kpi/models.py
+++ web/kpi/models.py
@@ -6,3 +6,20 @@ class DatastoreRecordCount(models.Model):
 
     def __unicode__(self):
         return u"%s - %d records" % (self.date, self.record_count)
+
+class MonthlyCounts(models.Model):
+    date = models.DateField() # first of the month
+
+    total_scrapers = models.IntegerField()
+    this_months_scrapers = models.IntegerField()
+
+    total_views = models.IntegerField()
+    this_months_views = models.IntegerField()
+
+    total_users = models.IntegerField()
+    this_months_users = models.IntegerField()
+
+    active_coders = models.IntegerField()
+    delta_active_coders = models.IntegerField()
+
+
