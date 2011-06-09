192925167aedc50ea4b74c68248ab6a1a60dd308
francis <francis@flourish.org>
2011-06-09 21:08:13 +0100

    Various fixes to codewiki tests.

--- web/codewiki/tests/viewseditor.py
+++ web/codewiki/tests/viewseditor.py
@@ -8,13 +8,10 @@ from django.test import TestCase
 import codewiki
 
 class ScraperViewsEditorTests(TestCase):
-    fixtures = ['test_data.json']
+    fixtures = ['./fixtures/test_data.json']
     
     def test_raw_content(self):
-        response = self.client.get(reverse('raw', kwargs={'wiki_type':'scraper', 'short_name': 'test_scraper'}))
+        response = self.client.get(reverse('raw', kwargs={'short_name': 'test_scraper'}))
         self.assertEqual(response.status_code, 200)
-
-    def test_this_fails(self):
-        raise Exception("failure")
 
   
