90cb4bb7dc44e712929637e92849fd3cbe88404c
francis <francis@flourish.org>
2011-06-09 21:20:02 +0100

    Default to only running tests in SCRAPERWIKI_APPS, but let it run specific others that are passed in.

--- web/scraperwiki_tests.py
+++ web/scraperwiki_tests.py
@@ -2,4 +2,8 @@ from django.test.simple import run_tests as default_run_tests
 from django.conf import settings 
 
 def run_tests(test_labels, *args, **kwargs): 
-    return default_run_tests(settings.SCRAPERWIKI_APPS, *args, **kwargs) 
+    # default to all ScraperWiki apps (so we don't run all the Django app tests etc.)
+    if len(test_labels) == 0:
+        test_labels = settings.SCRAPERWIKI_APPS
+
+    return default_run_tests(test_labels, *args, **kwargs) 
