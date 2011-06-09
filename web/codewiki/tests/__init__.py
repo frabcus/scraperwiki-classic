a3ddaf4b508c26e7a22abe44f6cdad8966f7e109
francis <francis@flourish.org>
2011-06-09 20:42:34 +0100

    Remove metadata import, that test no longer exists

--- web/codewiki/tests/__init__.py
+++ web/codewiki/tests/__init__.py
@@ -2,5 +2,4 @@ from codewiki.tests.views import *
 from codewiki.tests.viewseditor import *
 from codewiki.tests.util import *
 from codewiki.tests.management import *
-from codewiki.tests.metadata import *
 from codewiki.tests.run_events import *
