a9dba5fcfa74dd8b204dd175e5ade909dce419bc
francis <francis@flourish.org>
2011-06-20 13:20:46 -0400

    Beginnings of rollback edit mode

--- web/codewiki/viewseditor.py
+++ web/codewiki/viewseditor.py
@@ -158,10 +158,23 @@ def edit(request, short_name='__new__', wiki_type='scraper', language='python'):
             return HttpResponseRedirect(reverse("editor_edit", args=[scraper.wiki_type, short_name]))
         if not scraper.actionauthorized(request.user, "readcodeineditor"):
             return HttpResponseNotFound(render_to_string('404.html', scraper.authorizationfailedmessage(request.user, "readcodeineditor"), context_instance=RequestContext(request)))
-        
-        
-        status = scraper.get_vcs_status(-1)
-        assert 'currcommit' not in status 
+       
+        # link from history page can take us to "rollback" mode and see earlier revision
+        rollback_rev = request.GET.get('rollback_rev', None)
+        if rollback_rev is not None:
+            rollback_rev = int(rollback_rev)
+            assert rollback_rev >= 0 # too confusing for now otherwise!
+            # get revision *after* as we want the 'code' after the change
+            # (see below it uses prevcommit to get rev number)
+            get_rev = rollback_rev + 1
+            context['rollback_rev'] = rollback_rev
+        else:
+            # default to getting most recent revision
+            get_rev = -1
+        status = scraper.get_vcs_status(get_rev)
+        if rollback_rev is None:
+            assert 'currcommit' not in status 
+
         # assert not status['ismodified']  # should hold, but disabling it for now
         context['code'] = status["code"]
         context['rev'] = status.get('prevcommit',{}).get("rev") or 0
