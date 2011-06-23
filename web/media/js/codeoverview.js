70aac03400a9998a63534d85b77b64656f1c0931
Tom Mortimer-Jones <tom@morty.co.uk>
2011-06-23 11:11:55 +0100

    Remove redundent javascript code for the publish button that doesn't exist.

--- web/media/js/codeoverview.js
+++ web/media/js/codeoverview.js
@@ -204,22 +204,6 @@ function setupScraperEditInPlace(short_name)
                 return false;
            }
        );          
-      
-
-       $('#publishScraperButton').click(function(){
-           $.ajax({
-               url: 'admin/',
-               data: {'id': 'publishScraperButton'},
-               type: 'POST',
-               success: function(){
-                   $('#publishScraper').fadeOut();
-               },
-               error: function(){
-                   alert("Something went wrong publishing this scraper. Please try again. If the problem continues please send a message via the feedback form.");
-               }
-           });
-           return false;
-       });
 }
 
 
