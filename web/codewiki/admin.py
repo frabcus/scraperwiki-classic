98fac6aac423ef6f8227f99e4ae8fe5e953232f7
rossjones <devnull@localhost>
2011-07-06 11:15:11 +0100

    Added admin actions for views/scrapers to filter by featured status and also to update selected items

--- web/codewiki/admin.py
+++ web/codewiki/admin.py
@@ -15,6 +15,14 @@ class ScraperRunEventInlines(admin.TabularInline):
     model = ScraperRunEvent
     extra = 0
 
+def mark_featured(modeladmin, request, queryset):
+    queryset.update(featured=True)
+mark_featured.short_description = 'Mark selected items as featured'
+
+def mark_unfeatured(modeladmin, request, queryset):
+    queryset.update(featured=False)
+mark_unfeatured.short_description = 'Mark selected items as unfeatured'
+
 class CodeAdmin(admin.ModelAdmin):
     inlines = (UserCodeRoleInlines,)    
     readonly_fields = ('wiki_type','guid')
@@ -22,13 +30,14 @@ class CodeAdmin(admin.ModelAdmin):
 class ScraperAdmin(CodeAdmin):
     inlines = (UserCodeRoleInlines,)
     list_display = ('title', 'short_name', 'last_run', 'status', 'privacy_status')
-    list_filter = ('status', 'last_run', 'privacy_status')
+    list_filter = ('status', 'last_run', 'privacy_status', 'featured',)
     search_fields = ('title', 'short_name')
+    actions = [mark_featured, mark_unfeatured]
 
 class ViewAdmin(CodeAdmin):
-    list_filter = ('status', 'mime_type', 'privacy_status',)
+    list_filter = ('status', 'mime_type', 'privacy_status','featured',)
     search_fields = ('title', 'short_name')
-
+    actions = [mark_featured, mark_unfeatured]
 
 admin.site.register(Scraper, ScraperAdmin)
 admin.site.register(View, ViewAdmin)
