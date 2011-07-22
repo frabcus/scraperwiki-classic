ce24593f8116997640d32e229b3aeb9b09a28f9d
francis <francis@flourish.org>
2011-07-22 10:21:15 +0100

    Add verbosity

--- tests/selenium_runner.py
+++ tests/selenium_runner.py
@@ -1,8 +1,53 @@
 #!/usr/bin/env python
 
-import sys, unittest, imp, optparse
+import sys, unittest, imp, optparse, time
 from selenium_test import SeleniumTest
 import simplejson as json
+
+# This is a copy of unittest.TextTestRunner, that adds extra option of pausing
+# whenever there is an error. It uses _WritelnDecorator and _TextTestResult
+# from unittest, which might cause problems with future versions of Python.
+# (if so easiest thing may be to copy them from older verisons)
+class OurTextTestRunner:
+    """A test runner class that displays results in textual form.
+
+    It prints out the names of tests as they are run, errors as they
+    occur, and a summary of the results at the end of the test run.
+    """
+    def __init__(self, stream=sys.stderr, descriptions=1, verbosity=1):
+        self.stream = unittest._WritelnDecorator(stream)
+        self.descriptions = descriptions
+        self.verbosity = verbosity
+
+    def _makeResult(self):
+        return unittest._TextTestResult(self.stream, self.descriptions, self.verbosity)
+
+    def run(self, test):
+        "Run the given test case or test suite."
+        result = self._makeResult()
+        startTime = time.time()
+        test(result)
+        stopTime = time.time()
+        timeTaken = stopTime - startTime
+        result.printErrors()
+        self.stream.writeln(result.separator2)
+        run = result.testsRun
+        self.stream.writeln("Ran %d test%s in %.3fs" %
+                            (run, run != 1 and "s" or "", timeTaken))
+        self.stream.writeln()
+        if not result.wasSuccessful():
+            self.stream.write("FAILED (")
+            failed, errored = map(len, (result.failures, result.errors))
+            if failed:
+                self.stream.write("failures=%d" % failed)
+            if errored:
+                if failed: self.stream.write(", ")
+                self.stream.write("errors=%d" % errored)
+            self.stream.writeln(")")
+        else:
+            self.stream.writeln("OK")
+        return result
+
 
 #   eg:
 #
@@ -17,7 +62,10 @@ if __name__ == '__main__':
                       help="URL of the ScraperWiki web application to test, defaults to http://localhost:8000/",  
                       default="http://localhost:8000/", metavar="application url (string)")
 
-    parser.add_option("--tests", default="test_registration,test_scrapers", help="Comma separated list of modules to run tests from, defaults to 'test_registration,test_scrapers'")
+    parser.add_option("--tests", default="test_registration,test_scrapers", 
+                     help="Comma separated list of modules to run tests from, defaults to 'test_registration,test_scrapers'")
+    parser.add_option("--verbosity", dest="verbosity", action="store", default=1, type='int', 
+                     help="How much to display while running the tests, try 0, 1, 2. Default is 1.")
 
     parser.add_option("-s", "--seleniumhost", dest="shost", action="store", type='string',
                       help="The host that Selenium RC is running on",  
@@ -28,7 +76,8 @@ if __name__ == '__main__':
     parser.add_option("--username", help="Login to Selenium RC, if needed")
     parser.add_option("--accesskey", help="Access control to Selenium RC, if needeed")
     parser.add_option("--os", help="Operating system to run browser on, passed to Selenium RC, optional")
-    parser.add_option("--browser", default="*firefox", help="Which browser, e.g. *firefox, *chrome, *safari, *iexplore. Put in a bad value to see full list. Defaults to *firefox.")
+    parser.add_option("--browser", default="*firefox", 
+                      help="Which browser, e.g. *firefox, *chrome, *safari, *iexplore. Put in a bad value to see full list. Defaults to *firefox.")
     parser.add_option("--browserversion", help="Passed into selenium with browser parameter, optional")
     
     (options, args) = parser.parse_args()
@@ -46,19 +95,23 @@ if __name__ == '__main__':
                                                       "os":options.os, "browser":options.browser, "browser-version":options.browserversion })
     else:
         SeleniumTest._selenium_browser = options.browser
-    print "SeleniumRC %s:%d, ScraperWiki %s, Browser %s" % (SeleniumTest._selenium_host, SeleniumTest._selenium_port, SeleniumTest._app_url, str(SeleniumTest._selenium_browser))
 
-    if 'localhost' in options.url:
-        print '*' * 80
-        print 'If running tests locally, make sure that seleniumrc is running along with the\n\
-    local services inside the virtualenv'
-        print '*' * 80
+    if options.verbosity > 1:
+        print "SeleniumRC %s:%d, ScraperWiki %s, Browser %s" % (SeleniumTest._selenium_host, SeleniumTest._selenium_port, SeleniumTest._app_url, str(SeleniumTest._selenium_browser))
+        if 'localhost' in options.url:
+            print '*' * 80
+            print 'If running tests locally, make sure that seleniumrc is running along with the\n\
+        local services inside the virtualenv'
+            print '*' * 80
         
     for testsmodule in options.tests.split(","):
         module = imp.load_module(testsmodule, *imp.find_module(testsmodule))
-        print '\n%s\nRunning tests from module: %s\n' % ("="*80,repr(module))
+        if options.verbosity > 1:
+            print '\n%s\nRunning tests from module: %s\n' % ("="*80,module.__name__)
+        elif options.verbosity > 0:
+            print 'module %s' % (module.__name__)
         loader = unittest.TestLoader().loadTestsFromModule( module )
-        unittest.TextTestRunner( verbosity=2 ).run( loader )
+        OurTextTestRunner( verbosity=options.verbosity ).run( loader )
     
     
     
--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -12,7 +12,7 @@ class SeleniumTest(unittest.TestCase):
 
     _app_url = ''
 
-    def setUp(self):
+    def setUp(self, verbosity=1):
         self.verificationErrors = []
         self.selenium = selenium(SeleniumTest._selenium_host, SeleniumTest._selenium_port, 
                                  SeleniumTest._selenium_browser, SeleniumTest._app_url)
@@ -22,9 +22,12 @@ class SeleniumTest(unittest.TestCase):
         self.selenium.set_context("sauce:job-name=%s" % self._testMethodName)  
         self.selenium.window_maximize()
 
+        self.verbosity = verbosity
+
     def wait_for_page(self, doing=None):
         hit_limit = True
-        print "waiting_for_page", self.selenium.get_location()
+        if self.verbosity > 1:
+            print "  SeleniumTest: waiting_for_page", self.selenium.get_location()
         try:
             self.selenium.wait_for_page_to_load('30000')
             hit_limit = False
