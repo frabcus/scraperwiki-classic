d4fe52a6f8ac8538fb8ba8044206727f89509ec0
francis <francis@flourish.org>
2011-07-28 15:19:09 +0100

    Rename External API (again)

--- web/documentation/titles.py
+++ web/documentation/titles.py
@@ -11,19 +11,19 @@ page_titles = {
     
     "ruby_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already code Ruby"),
     "ruby_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for Ruby programmers"),
-    "ruby_help_documentation" : ("ScraperWiki library","All the functions in the Ruby ScraperWiki module"),
+    "ruby_help_documentation" : ("ScraperWiki library functions","All the functions in the Ruby ScraperWiki module"),
     "ruby_libraries" : ("3rd party libraries","Ruby batteries included in ScraperWiki"),
     "ruby_view_guide" : ("Views tutorial","How to use ScraperWiki views in Ruby"),
    
     "python_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already program in Python"),
     "python_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for Python programmers"),
-    "python_help_documentation" : ("ScraperWiki library","All the functions in the Python scraperwiki module"),
+    "python_help_documentation" : ("ScraperWiki library functions","All the functions in the Python scraperwiki module"),
     "python_libraries" : ("3rd party libraries","Python batteries included in ScraperWiki"),
     "python_view_guide" : ("Views tutorial","How to use ScraperWiki views in Python"),
     
     "php_intro_tutorial" : ("Introductory tutorial","How to use ScraperWiki for people who can already write PHP"),
     "php_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for PHP programmers"),
-    "php_help_documentation" : ("ScraperWiki library","All the functions in the PHP scraperwiki namespace"),
+    "php_help_documentation" : ("ScraperWiki library functions","All the functions in the PHP scraperwiki namespace"),
     "php_libraries" : ("3rd party libraries","PHP batteries included in ScraperWiki"),
     "php_view_guide" : ("Views tutorial","How to use ScraperWiki views in PHP"),
     
