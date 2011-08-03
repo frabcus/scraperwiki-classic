1fdba55d571d54f83f4f1d2c64317669caa600dc
rossjones <devnull@localhost>
2011-08-03 13:41:01 +0100

    Removing some code

--- web/codewiki/templatetags/screen_shot.py
+++ web/codewiki/templatetags/screen_shot.py
@@ -8,12 +8,12 @@ register = Library()
 @register.inclusion_tag('codewiki/templatetags/screenshot.html')
 def screen_shot(code_object, size='medium'):
     
-    if code_object.has_screenshot(size=size):
-        has_screenshot = True
+    has_screenshot = code_object.has_screenshot(size=size):
+    if has_screenshot:
         url = settings.MEDIA_URL + 'screenshots/' + size + '/' + code_object.get_screenshot_filename(size=size)
     else:
-        has_screenshot = False
         url = settings.MEDIA_URL + 'images/testcard_' + size + '.png'
+        
     return {
         'url': url,
         'has_screenshot': has_screenshot,        
