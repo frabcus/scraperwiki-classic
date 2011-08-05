981ac548cfb24f24625be7bba60676d52db19b0c
rossjones <devnull@localhost>
2011-08-05 11:33:53 +0100

    Fixed silly typo

--- web/codewiki/views.py
+++ web/codewiki/views.py
@@ -244,7 +244,7 @@ def scraper_admin_controleditors(request, short_name):
     # If there is no role and we are the user that is applying this (i.e. to ourselves)
     # then we can remove the role. Otherwise check they already are a role.
     if newrole == '' and request.user == roleuser:
-        scraper.set_user_role(request.user, 'editor', remove=True):
+        scraper.set_user_role(request.user, 'editor', remove=True)
     elif models.UserCodeRole.objects.filter(code=scraper, user=roleuser, role=newrole):
         return HttpResponse("Warning: user is already '%s'" % newrole)
     
