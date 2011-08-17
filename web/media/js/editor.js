c6a21296ad8b1e31e46d5083f5b092105d178295
francis <francis@flourish.org>
2011-08-17 15:53:03 +0100

    Always open preview in same new window

--- web/media/js/editor.js
+++ web/media/js/editor.js
@@ -1157,7 +1157,7 @@ writeToChat("<b>requestededitcontrol: "+data.username+ " has requested edit cont
 
         previewscreen = ['<h3>View preview'];
         if (viewurl)
-             previewscreen.push(' (<a href="'+viewurl+'" target="_blank">open in new window</a>'+previewmessage+')'); 
+             previewscreen.push(' (<a href="'+viewurl+'" target="scraperwiki_preview_' + short_name + '">open in new window</a>'+previewmessage+')'); 
         previewscreen.push('</h3>'); 
         isrc = ""; // isrc = viewurl; (would allow direct inclusion from saved version)
                    // force the preview iframe to fill most of what it should.  needs more work
