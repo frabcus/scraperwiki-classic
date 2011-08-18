b8ccc44ac25fa1ee78351136911b7b4dde3a15a1
francis <francis@flourish.org>
2011-08-18 20:09:05 +0100

    Whoops, lost lxml link before by accident

--- web/documentation/titles.py
+++ web/documentation/titles.py
@@ -14,21 +14,21 @@ page_titles = {
     "ruby_help_documentation" : ("ScraperWiki library functions","All the functions in the Ruby ScraperWiki module"),
     "ruby_libraries" : ("3rd party libraries","Ruby batteries included in ScraperWiki"),
     "ruby_view_guide" : ("First view tutorial","How to use ScraperWiki views in Ruby"),
-    "ruby_csv_guide" : ("CSV copy & paste guide","Getting data from comma-separated value files in Ruby"),
+    "ruby_csv_guide" : ("CSV topic guide","Getting data from comma-separated value files in Ruby"),
    
     "python_intro_tutorial" : ("First scraper tutorial","How to use ScraperWiki for people who can already program in Python"),
     "python_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for Python programmers"),
     "python_help_documentation" : ("ScraperWiki library functions","All the functions in the Python scraperwiki module"),
     "python_libraries" : ("3rd party libraries","Python batteries included in ScraperWiki"),
     "python_view_guide" : ("First view tutorial","How to use ScraperWiki views in Python"),
-    "python_csv_guide" : ("CSV copy & paste guide","Getting data from comma-separated value files in Python"),
+    "python_csv_guide" : ("CSV topic guide","Getting data from comma-separated value files in Python"),
     
     "php_intro_tutorial" : ("First scraper tutorial","How to use ScraperWiki for people who can already write PHP"),
     "php_datastore_guide" : ("Datastore copy & paste guide","A guide to the ScraperWiki SQL datastore for PHP programmers"),
     "php_help_documentation" : ("ScraperWiki library functions","All the functions in the PHP scraperwiki namespace"),
     "php_libraries" : ("3rd party libraries","PHP batteries included in ScraperWiki"),
     "php_view_guide" : ("First view tutorial","How to use ScraperWiki views in PHP"),
-    "php_csv_guide" : ("CSV copy & paste guide","Getting data from comma-separated value files in PHP"),
+    "php_csv_guide" : ("CSV topic guide","Getting data from comma-separated value files in PHP"),
     
     "technical_overview" : ("Technical overview","All the component parts of ScraperWiki and how they work together"),    
 }
