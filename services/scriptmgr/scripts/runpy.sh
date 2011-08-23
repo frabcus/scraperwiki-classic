b8fcd599e7e60bf0a3d27cf0d01e6188646fa248
rossjones <devnull@localhost>
2011-08-23 12:03:02 +0100

    Updated

--- services/scriptmgr/scripts/runpy.sh
+++ services/scriptmgr/scripts/runpy.sh
@@ -1,3 +1,3 @@
 route add default gw 10.0.0.1 eth0
-su - scriptrunner -c "cd ~;exec.py -script=script.py -ds=$1 -runid=$2"
+su - scriptrunner -c "cd ~;/home/startup/exec.py -script=script.py -ds=$1 -runid=$2"
 
--- services/scriptmgr/scripts/runrb.sh
+++ services/scriptmgr/scripts/runrb.sh
@@ -1,2 +1,2 @@
 route add default gw 10.0.0.1 eth0
-su - scriptrunner -c "cd ~;exec.rb --script=script.py --ds=$1 --runid=$2;"
+su - scriptrunner -c "cd ~;/home/startup/exec.rb --script=script.py --ds=$1 --runid=$2;"
