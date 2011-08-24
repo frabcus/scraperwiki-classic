33337520577e52eff4c780362d9aa525973232c0
rossjones <devnull@localhost>
2011-08-24 15:32:52 +0100

    Updated scriptmgr to fix problem with wrong IP address being requested

--- uml/dataproxy/dataproxy.py
+++ uml/dataproxy/dataproxy.py
@@ -74,9 +74,6 @@ class ProxyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             
         rem       = self.connection.getpeername()
         loc       = self.connection.getsockname()
-        self.logger.info(rem[0])
-        if lxc:
-            self.logger.info( host[0:host.find(':')] )
         if lxc and rem[0] == host[0:host.find(':')]:
             lident = urllib.urlopen ('http://%s/Ident?%s:%s' % (host, rem[0], loc[1])).read()               
         else:
