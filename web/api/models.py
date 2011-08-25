e16347721641272a6498a4f4ba7e79505772e691
francis <francis@flourish.org>
2011-08-25 13:56:25 +0100

    Remove unused api_key model

--- web/api/models.py
+++ web/api/models.py
@@ -6,25 +6,3 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 
-class api_key(models.Model):
-    user = models.ForeignKey(User)
-    key = models.CharField(blank=True, max_length=32)
-    active = models.BooleanField(default=True)
-    description = models.TextField(null=False, blank=False)
-    
-    def __unicode__(self):
-      return "%s" % self.key
-    
-    def save(self):
-        if not self.key:
-            import hashlib
-            unique_string = "**@@@".join([
-                settings.SECRET_KEY, 
-                str(self.user.id), 
-                str(datetime.datetime.today())])
-            
-            key = hashlib.md5("%s" % unique_string).hexdigest()
-            self.key = key[:32]
-
-
-        super(api_key, self).save()
