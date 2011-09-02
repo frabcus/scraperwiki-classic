4e8dfbb7fc6be93c19769be7e8d73fedafa5ad9c
rossjones <devnull@localhost>
2011-09-02 17:08:20 +0100

    Setting ulimit -t

--- services/scriptmgr/scripts/runpy.sh
+++ services/scriptmgr/scripts/runpy.sh
@@ -1,5 +1,5 @@
 route add default gw 10.0.0.1 eth0 > /dev/null
 export PYTHONPATH='/home/scraperwiki/python'
 export PYTHONUNBUFFERED=true
-su scriptrunner -c "cd ~;/home/startup/exec.py --script /home/scriptrunner/script.py --ds $1 --runid $2"
+su scriptrunner -c "cd ~;ulimit -t 80;/home/startup/exec.py --script /home/scriptrunner/script.py --ds $1 --runid $2"
 
