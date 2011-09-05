7ff374fbbb76ef1eebc5806023c3b3b3142971a1
rossjones <devnull@localhost>
2011-09-05 18:47:50 +0100

    More forgotten bashisms

--- services/scriptmgr/scripts/runjs.sh
+++ services/scriptmgr/scripts/runjs.sh
@@ -1,7 +1,7 @@
 route add default gw 10.0.0.1 eth0 > /dev/null
 export NODE_PATH='/usr/local/lib/node_modules:/home/scraperwiki/javascript:/home/scriptrunner'
 SN = $3
-if [ -z $3 ]
-	SN = ""
+if [ -z $3 ]; then 
+	$SN = ""
 fi
 su scriptrunner -c "cd ~;ulimit -t 80;/home/startup/exec.js --script /home/scriptrunner/script.js --ds $1 --runid $2 --scrapername $SN"
