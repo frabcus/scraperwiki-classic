f4dd900bbf5005ee62b7a7ba829abc4323667fc0
rossjones <devnull@localhost>
2011-09-05 10:57:17 +0100

    Updated the Javascript executor

--- services/scriptmgr/scripts/exec.js
+++ services/scriptmgr/scripts/exec.js
@@ -13,22 +13,10 @@ var options = [
 opts.parse(options);
 
 /*
-if options.gid:
-    os.setregid(int(options.gid), int(options.gid))
-if options.uid:
-    os.setreuid(int(options.uid), int(options.uid))
-if options.path:
-    sys.path.append( options.path )
-*/
 
-
-/*host, port = string.split(options.ds, ':')
+host, port = string.split(options.ds, ':')
 scraperwiki.datastore.create(host, port, options.scrapername or "", options.runid)
-
 scraperwiki.logfd = sys.stderr
-
-# in the future can divert to webproxy
-#scraperwiki.utils.urllibSetup(http_proxy='http://127.0.0.1:9002')
 */
 
 process.on('SIGXCPU', function () {
@@ -36,21 +24,14 @@ process.on('SIGXCPU', function () {
 });
 
 try {
+	// Load and run the script provided to us
 	require( opts.get('script') );
-	//var script = require( opts.get('script'));
-	//script.main();
 } catch( err ) {
 	console.log( err );
+	/*
+		Need to better handle the stacktrace
+	    etb = scraperwiki.stacktrace.getExceptionTraceback(code)  
+	    assert etb.get('message_type') == 'exception'
+	    scraperwiki.dumpMessage(etb)
+	*/
 }
-/*
-code = open(options.script).read()
-try:
-    import imp
-    mod = imp.new_module('scraper')
-    exec code.rstrip() + "\n" in mod.__dict__
-
-except Exception, e:
-    etb = scraperwiki.stacktrace.getExceptionTraceback(code)  
-    assert etb.get('message_type') == 'exception'
-    scraperwiki.dumpMessage(etb)
-*/
