9aaac57fd3acbe4c5367e29e8cd23128c685b603
rossjones <devnull@localhost>
2011-09-05 18:50:20 +0100

    Updated with debugging

--- services/scriptmgr/scripts/runjs.sh
+++ services/scriptmgr/scripts/runjs.sh
@@ -1,7 +1,9 @@
 route add default gw 10.0.0.1 eth0 > /dev/null
 export NODE_PATH='/usr/local/lib/node_modules:/home/scraperwiki/javascript:/home/scriptrunner'
 SN=$3
-if [ -z $3 ]; then 
+echo "SN is $SN"
+if [ -z $SN ]; then 
+	echo "SN is not set"	
 	SN=""
 fi
 su scriptrunner -c "cd ~;ulimit -t 80;/home/startup/exec.js --script /home/scriptrunner/script.js --ds $1 --runid $2 --scrapername $SN"
