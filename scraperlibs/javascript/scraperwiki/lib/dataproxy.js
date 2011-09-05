1c08c96cf77eb8d6048ebc66d90bb046f463de92
rossjones <devnull@localhost>
2011-09-05 20:49:44 +0100

    Updated with callbacks

--- scraperlibs/javascript/scraperwiki/lib/dataproxy.js
+++ scraperlibs/javascript/scraperwiki/lib/dataproxy.js
@@ -64,14 +64,85 @@ DataProxyClient.prototype.save = function(indices, data, verbose, callback) {
 	var self = this;
 	this.ensureConnected(function(ok){
 		if ( ok ) {
-			internal_save(indices,data,verbose);
-			callback( "DataProxy (" + self.host + ":" + self.port + " - " +  self.scrapername +")" );
+			var result = internal_save(indices,data,verbose, function(result){
+				callback( result );	
+			});
+			
 		}
 	});
 }
 
 function internal_save(callback) {
 	console.log( 'internal save ')
+	callback( 'status' )
+	;
+	/*
+    if unique_keys != None and type(unique_keys) not in [ list, tuple ]:
+        raise databaseexception({ "error":'unique_keys must a list or tuple', "unique_keys_type":str(type(unique_keys)) })
+
+    def convdata(unique_keys, scraper_data):
+        if unique_keys:
+            for key in unique_keys:
+                if key not in scraper_data:
+                    return { "error":'unique_keys must be a subset of data', "bad_key":key }
+                if scraper_data[key] == None:
+                    return { "error":'unique_key value should not be None', "bad_key":key }
+        jdata = { }
+        for key, value in scraper_data.items():
+            if not key:
+                return { "error": 'key must not be blank', "bad_key":key }
+            if type(key) not in [unicode, str]:
+                return { "error":'key must be string type', "bad_key":key }
+            if not re.match("[a-zA-Z0-9_\- ]+$", key):
+                return { "error":'key must be simple text', "bad_key":key }
+            
+            if type(value) in [datetime.datetime, datetime.date]:
+                value = value.isoformat()
+            elif value == None:
+                pass
+            elif isinstance(value, SqliteError):
+                return {"error": str(value)}
+            elif type(value) == str:
+                try:
+                    value = value.decode("utf-8")
+                except:
+                    return {"error": "Binary strings must be utf-8 encoded"}
+            elif type(value) not in [int, bool, float, unicode, str]:
+                value = unicode(value)
+            jdata[key] = value
+        return jdata
+            
+
+    if type(data) == dict:
+        rjdata = convdata(unique_keys, data)
+        if rjdata.get("error"):
+            raise databaseexception(rjdata)
+        if date:
+            rjdata["date"] = date
+    else:
+        rjdata = [ ]
+        for ldata in data:
+            ljdata = convdata(unique_keys, ldata)
+            if ljdata.get("error"):
+                raise databaseexception(ljdata)
+            rjdata.append(ljdata)
+    result = scraperwiki.datastore.request({"maincommand":'save_sqlite', "unique_keys":unique_keys, "data":rjdata, "swdatatblname":table_name})
+
+    if "error" in result:
+        raise databaseexception(result)
+
+    if verbose >= 2:
+        pdata = {}
+        if type(data) == dict:
+            for key, value in data.items():
+                pdata[strencode_trunc(key, 50)] = strencode_trunc(value, 50)
+        elif data:
+            for key, value in data[0].items():
+                pdata[strencode_trunc(key, 50)] = strencode_trunc(value, 50)
+            pdata["number_records"] = "Number Records: %d" % len(data)
+        scraperwiki.dumpMessage({'message_type':'data', 'content': pdata})
+    return result
+*/		
 };
 
 
