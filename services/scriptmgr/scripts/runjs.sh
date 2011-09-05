c27b7941222a52a676bae61e91a5fbe99ff91c58
rossjones <devnull@localhost>
2011-09-05 18:46:33 +0100

    More changes to run the js script

--- services/scriptmgr/scripts/runjs.sh
+++ services/scriptmgr/scripts/runjs.sh
@@ -1,3 +1,7 @@
 route add default gw 10.0.0.1 eth0 > /dev/null
 export NODE_PATH='/usr/local/lib/node_modules:/home/scraperwiki/javascript:/home/scriptrunner'
-su scriptrunner -c "cd ~;ulimit -t 80;/home/startup/exec.js --script /home/scriptrunner/script.js --ds=$1 --runid=$2 --scrapername=$3"
+SN = $3
+if [ -z $3 ]
+	SN = ""
+fi
+su scriptrunner -c "cd ~;ulimit -t 80;/home/startup/exec.js --script /home/scriptrunner/script.js --ds $1 --runid $2 --scrapername $SN"
