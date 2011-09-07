9ee424e29f75b3ea83ed2b3b3c12850bd552c976
rossjones <devnull@localhost>
2011-09-07 14:48:49 +0100

    Logging ident output from DP

--- uml/dataproxy/dataproxy.py
+++ uml/dataproxy/dataproxy.py
@@ -81,7 +81,8 @@ class ProxyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             via    = config.get(uml, 'via' )
             lident = urllib.urlopen ('http://%s:%s/Ident?%s:%s' % (uml_host, via, port, loc[1])).read()   
 
-                # should be using cgi.parse_qs(query) technology here
+                # should be using cgi.parse_qs(query) technology here                
+        self.logger.debug("LIDENT: %s" % (line,) )                                
         for line in lident.split('\n'):
             if line:
                 key, value = line.split('=')
