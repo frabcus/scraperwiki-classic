5aa3fbf525478e08aebd560c0e5aee52074dd31f
rossjones <devnull@localhost>
2011-09-07 21:32:42 +0100

    Redirecting stdout

--- services/scriptmgr/scripts/exec.php
+++ services/scriptmgr/scripts/exec.php
@@ -6,7 +6,10 @@ foreach (split(':', getenv('PHPPATH')) as $dir)
     ini_set('include_path',  ini_get('include_path') . PATH_SEPARATOR . $dir) ;
 
 $logfd = STDOUT; // fopen("php://fd/3", "w") ;
-$STDERR = STDOUT;
+
+fclose(STDERR);
+$STDERR = fopen('php://stdout', 'w');
+
 
 require_once 'scraperwiki.php';
 require_once 'scraperwiki/datastore.php';
