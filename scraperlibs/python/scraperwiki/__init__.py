c95f58beee1d948474b28653190341b75ba8c630
rossjones <devnull@localhost>
2011-09-07 16:27:03 +0100

    Added writeBinaryData method

--- scraperlibs/python/scraperwiki/__init__.py
+++ scraperlibs/python/scraperwiki/__init__.py
@@ -1,3 +1,5 @@
+import base64
+
 try    : import json
 except : import simplejson as json
 
@@ -7,6 +9,12 @@ logfd = None   # set to os.fdopen(3, 'w', 0) for consuming json objects
 def dumpMessage(d):
     val = json.dumps(d)
     logfd.write( "%s\n" % (val,) )
+    logfd.flush()
+
+
+def writeBinaryData(data):
+    msg = {'message_type': 'console', 'encoding': 'base64', 'content': base64.encodestring(data)})
+    logfd.write( "%s\n" % (json.dumps(msg),) )
     logfd.flush()
 
 
