2be8e2fc7a3ea956ccd987ac59a1c5985d1b8b87
rossjones <devnull@localhost>
2011-09-08 16:15:00 +0100

    Updated to make sure data proxy sends header request

--- scraperlibs/python/scraperwiki/datastore.py
+++ scraperlibs/python/scraperwiki/datastore.py
@@ -68,6 +68,7 @@ def ensure_connected():
         data["vrunid"] = m_runid
         data["attachables"] = " ".join(attachables)
         m_socket.sendall('GET /?%s HTTP/1.1\n\n' % urllib.urlencode(data))
+        m_socket.sendall('Host: %s\n\n' % (m_host,))
         line = receiveoneline(m_socket)  # comes back with True, "Ok"
         res = json.loads(line)
         assert res.get("status") == "good", res
