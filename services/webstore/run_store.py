d486930ce21aef1c8108b7b4ddd74871f34d3f30
rossjones <devnull@localhost>
2011-09-13 11:44:49 +0100

    changing code to activate the virtual env

--- services/webstore/run_store.py
+++ services/webstore/run_store.py
@@ -1,3 +1,8 @@
+# Activate the virtual env
+instance_dir = '/var/www/scraperwiki/'
+pyenv_bin_dir = os.path.join(instance_dir, 'bin')
+activate_this = os.path.join(pyenv_bin_dir, 'activate_this.py')
+execfile(activate_this, dict(__file__=activate_this))
 
-from webstore.web import app
-app.run( '0.0.0.0', 2112 )
+from webstore.web import app as application
+application.run( '0.0.0.0', 2112 )
