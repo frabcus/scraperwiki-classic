3996ea4b427c43722f61b837820d94045f77a66e
rossjones <devnull@localhost>
2011-09-14 10:10:40 +0100

    updated httpproxy logging

--- uml/httpproxy/httpproxy.py
+++ uml/httpproxy/httpproxy.py
@@ -380,6 +380,7 @@ class HTTPProxyHandler (BaseHTTPServer.BaseHTTPRequestHandler) :
         (scheme, netloc, path, params, query, fragment) = urlparse.urlparse (self.path, 'http')
         isSW = netloc.startswith('127.0.0.1') or netloc.endswith('scraperwiki.com')
         isLocal = netloc.startswith('10.0.1')
+        print 'IsLocal: %s' % str(isLocal)
         
         #  Path /Status returns status information.
         #
