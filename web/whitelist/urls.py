8e74c3bdd7b37c2e4eb3c46ff04bc1624da51a84
rossjones <devnull@localhost>
2011-09-25 18:47:54 +0100

    removing whitelist app

--- web/whitelist/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from whitelist import views
-
-urlpatterns = patterns('',
-   url(r'^$',      views.whitelist_user, name="whitelist_user"),
-   url(r'^config.json$', views.whitelist_config_json, name="whitelist_config_json"),      
-   url(r'^config$', views.whitelist_config, name="whitelist_config"),
-   )
