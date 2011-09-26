eb58ee781bfbac47bd21360668725becf8468b43
Aidan Hobson Sayers <aidanhs@live.co.uk>
2011-09-26 17:59:32 +0100

    Added api key support to userinfo

--- web/api/viewshandlers.py
+++ web/api/viewshandlers.py
@@ -22,6 +22,7 @@ from django.utils.encoding import smart_str
 from django.core.serializers.json import DateTimeAwareJSONEncoder
 from django.utils import simplejson
 
+from frontend.models import UserProfile
 
 from codewiki.models import Scraper, Code, ScraperRunEvent, CodePermission, scraper_search_query, scrapers_overdue
 from codewiki.managers.datastore import DataStore
@@ -402,17 +403,29 @@ def usersearch_handler(request):
 
 
 def userinfo_handler(request):
-    username = request.GET.get('username', "") 
+    username = request.GET.get('username', "")
+    apikey = request.GET.get('apikey', "")
     users = User.objects.filter(username=username)
     result = [ ]
     for user in users:  # list of users is normally 1
         info = { "username":user.username, "profilename":user.get_profile().name }
         info["datejoined"] = user.date_joined.isoformat()
         info['coderoles'] = { }
-        for ucrole in user.usercoderole_set.exclude(code__privacy_status="deleted").exclude(code__privacy_status="private"):
-            if ucrole.role not in info['coderoles']:
-                info['coderoles'][ucrole.role] = [ ]
-            info['coderoles'][ucrole.role].append(ucrole.code.short_name)
+        for ucrole in user.usercoderole_set.exclude(code__privacy_status="deleted"):
+            if ucrole.code.privacy_status != "private":
+                if ucrole.role not in info['coderoles']:
+                    info['coderoles'][ucrole.role] = [ ]
+                info['coderoles'][ucrole.role].append(ucrole.code.short_name)
+            elif apikey:
+                try:
+                    api_user = UserProfile.objects.get(apikey=apikey).user
+                    if api_user.usercoderole_set.filter(code__short_name=ucrole.code.short_name):
+                        if ucrole.role not in info['coderoles']:
+                            info['coderoles'][ucrole.role] = [ ]
+                        info['coderoles'][ucrole.role].append(ucrole.code.short_name) 
+                except UserProfile.DoesNotExist:
+                    pass
+                
 
         result.append(info)
     
