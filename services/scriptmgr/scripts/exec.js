54ab92dc12df694c5390e0977a13c7c482740bcb
rossjones <devnull@localhost>
2011-09-27 17:08:14 +0100

    updated exec.js

--- services/scriptmgr/scripts/exec.js
+++ services/scriptmgr/scripts/exec.js
@@ -1,22 +1,25 @@
 #!/usr/local/bin/node
 
+var fs = require('fs');
+
 var opts = require('opts');
 var options = [
   {  long        : 'script', value : true },
-  {  long        : 'ds', value : true  },
-  {  long        : 'gid', value : true  },
-  {  long        : 'uid', value : true  },
-  {  long        : 'scrapername', value : true  },
   {  long        : 'attachables', value : true  },
-  {  long        : 'qs', value : true  },
-  {  long        : 'runid', value : true  },
-  {  long        : 'path', value : true  }
 ];
 opts.parse(options, true);
 
+var launch = JSON.parse( fs.readFileSync('launch.json', 'utf-8') );
+var datastore = launch['datastore'];
+var runid = launch['runid'];
+var scrapername = launch['scrapername'];
+var querystring = launch['querystring'];
+
+# TODO: Set querystring
+
 var sw = require('scraperwiki');
-var parts = opts.get('ds').split(':');
-sw.sqlite.init(parts[0], parts[1], opts.get("scrapername") || "", opts.get("runid") || "");
+var parts = datastore.split(':');
+sw.sqlite.init(parts[0], parts[1], scrapername, runid);
 
 process.on('SIGXCPU', function () {
 	throw 'ScraperWiki CPU time exceeded';
