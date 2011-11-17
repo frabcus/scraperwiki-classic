20900463a1a640dd2e731bc79335c47a70e9fefc
David Jones <david@scraperwiki.com>
2011-11-17 14:23:38 +0000

    Ticket https://bitbucket.org/ScraperWiki/scraperwiki/issue/92/
    Bugfix scraperwiki.scrape; so that it at least works minimally.

--- scraperlibs/javascript/scraperwiki/lib/scraperwiki.js
+++ scraperlibs/javascript/scraperwiki/lib/scraperwiki.js
@@ -1,12 +1,15 @@
+// ScraperWiki language support for JavaScript scrapers.
+// David Jones, ScraperWiki Limited
+
+// http://jsdom.org/
 var jsdom = require('jsdom');
 var request = require('request');
 var dp = require('./dataproxy');
 var fs = require('fs');
+var path  = require('path');
 
 exports.version = '1.0.0';
 exports.sqlite = new DataProxyClient();
-
-var jquery;
 
 exports.dumpMessage = dumpMessage = function(msg) {
 	console.log( "JSONRECORD(" + msg.length.toString() + "):" + msg.toString() + "\n" );
@@ -38,23 +41,19 @@ exports.parseError = parseError = function(err) {
 
 
 exports.scrape = function( url, func ) {
-	if ( !jquery ) {
-	 	jquery = fs.readFileSync("./jquery.1.7.0.min.js").toString();	
-		console.log('Loaded jQuery because we have not loaded it before')
-	}
-	
+    var jp = path.join(__dirname, "jquery.1.7.0.min.js");
+
     request({ uri: url }, function (error, response, body) {
         if (error && response.statusCode !== 200) {
             console.log('Error when contacting site')
         } else {
             jsdom.env({
                   html: body,
-                  scripts: jquery
+                  scripts: [ jp ],
                 }, 
-
                 function (err, window) {
-                   var $ = window.jQuery;
-                   func($, body);
+                   var $ = window.$;
+                   func($, body, err);
                 }
             );
         }
