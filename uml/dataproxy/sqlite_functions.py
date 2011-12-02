943405dcf902593518e6ec61b2c2abbc7298b51c
rossjones <devnull@localhost>
2011-12-02 16:22:41 +0000

    Added new file to hold sqlite functions

---
+++ uml/dataproxy/sqlite_functions.py
@@ -0,0 +1,6 @@
+"""
+    Simple functions added to extend the sqlite3 query language. 
+
+    By calling create_function on the sqlite3 connection object
+    it is possible to provide extra functionality to the dataproxy.
+"""
