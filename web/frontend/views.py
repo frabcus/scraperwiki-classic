e6ea63648efecea7247d80ee3d5db616ed75e547
zarino <devnull@localhost>
2011-12-20 22:23:38 +0000

    tweaks to Nicar event page, as per IRE feedback

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -391,7 +391,7 @@ def get_involved(request):
 
 
 def events(request, e=''):
-    names = ['jdcny','nicar']
+    names = ['jdcny','ltdmo']
     if e in names:
         url = 'frontend/events/%s.html' % e
         data = {}
