34f2b8c14b6ed6e2b77a84c6435bb4858a28571f
zarino <devnull@localhost>
2012-01-04 15:29:45 +0000

    trying to ajax in the data url manually, to see whether its the server or DataTables at fault

--- web/media/js/codeoverview_new.js
+++ web/media/js/codeoverview_new.js
@@ -666,6 +666,17 @@ function setDataPreview(table_name, table_schema){
                            column_names: column_names}));
 
 	console.log('#id_data_base = ' + $('#id_data_base').val());
+	
+	$.ajax({
+		type: 'GET',
+		url: $('#id_data_base').val() + 'swdata/?sEcho=1&iColumns=2&sColumns=&iDisplayStart=0&iDisplayLength=10&mDataProp_0=0&mDataProp_1=1&sSearch=&bRegex=false&sSearch_0=&bRegex_0=false&bSearchable_0=true&sSearch_1=&bRegex_1=false&bSearchable_1=true&iSortingCols=1&iSortCol_0=0&sSortDir_0=asc&bSortable_0=true&bSortable_1=true&_=1325690712712'
+		success: function(data){
+            console.log(data);
+        }, error: function(){
+            console.log('Error ajaxing ' + $('#id_data_base').val() + '...');
+        },
+        cache: false
+	});
 
     var dt = $('#datapreviews #data_preview_'+table_name+' table').dataTable( {
         "bProcessing": true,
