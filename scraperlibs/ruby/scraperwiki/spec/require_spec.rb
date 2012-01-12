d814e8ca8184237e984db503f37572409830f156
ehg <chris.blower@jumpwire.co.uk>
2012-01-12 14:38:09 +0000

    Added VCR test

--- scraperlibs/ruby/scraperwiki/spec/require_spec.rb
+++ scraperlibs/ruby/scraperwiki/spec/require_spec.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + '/helper'
 
+WebMock.allow_net_connect!
+
 describe Kernel do 
   context "When we include scraper_require" do
     before do
@@ -7,9 +9,17 @@ describe Kernel do
     end
 
     context "When we require a scraper" do
+      before do
+        @scraper_name = 'require_test'
+        require "scrapers/#{@scraper_name}"
+      end
+
+      it "fetches the scraper's code from ScraperWiki" do
+        a_request(:get, "https://scraperwiki.com/editor/raw/#{@scraper_name}").should have_been_made
+      end
+
       it "loads the scraper as a module" do
-        lambda { require 'scrapers/test' }.should_not raise_error
-        Test.should_not be_nil
+        RequireTest.should_not be_nil
       end
     end
 
