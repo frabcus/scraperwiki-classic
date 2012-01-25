47a1099dca4e6a51943b37db35ab513eaefb1435
David Jones <david@scraperwiki.com>
2012-01-25 17:34:06 +0000

    fixing all sorts of minor testy problems

--- web/codewiki/tests/views.py
+++ web/codewiki/tests/views.py
@@ -11,15 +11,15 @@ class ScraperViewsTests(TestCase):
     fixtures = ['test_data.json']
     
     def test_scraper_list(self):
-        """
-        test
+        """Tests that the list of scrapers can be retrieved.
         """
         response = self.client.get(reverse('scraper_list'))
         self.assertEqual(response.status_code, 200)
         
     def test_scraper_overview(self):
         response = self.client.get(reverse('code_overview', 
-                            kwargs={'wiki_type':'scraper', 'short_name': 'test_scraper'}))
+                            kwargs={'wiki_type':'scraper',
+                                    'short_name': 'test_scraper'}))
         self.assertEqual(response.status_code, 200)
     
     
@@ -47,7 +47,8 @@ class ScraperViewsTests(TestCase):
         self._ensure_repo_exists( 'test_scraper')
             
         response = self.client.get(reverse('scraper_history',
-                            kwargs={'wiki_type':'scraper', 'short_name': 'test_scraper'}))
+                            kwargs={'wiki_type':'scraper',
+                                    'short_name': 'test_scraper'}))
         self.assertEqual(response.status_code, 200)
         
     
@@ -55,7 +56,8 @@ class ScraperViewsTests(TestCase):
         self._ensure_repo_exists( 'test_scraper')
             
         response = self.client.get(reverse('scraper_comments',
-                            kwargs={'wiki_type':'scraper', 'short_name': 'test_scraper'}))
+                            kwargs={'wiki_type':'scraper',
+                                    'short_name': 'test_scraper'}))
         self.assertEqual(response.status_code, 200)
 
     def test_scraper_all_tags(self):
