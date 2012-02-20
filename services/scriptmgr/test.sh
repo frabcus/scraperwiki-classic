f5f5f8d33fc1690763c65d7044d967e7776c3fd3
David Jones <david@scraperwiki.com>
2012-02-20 17:07:45 +0000

    pass JSON object

--- services/scriptmgr/test.sh
+++ services/scriptmgr/test.sh
@@ -16,7 +16,17 @@ esac
 for i in $(awk 'BEGIN{for(i=1;i<='$count';++i)print i}')
 do
     echo 'Running' $i
-    data="code=import time;print 1-2;time.sleep(10);print 'hello'&run_id=$i&scrapername=test&scraper_id=$i&language=python"
+    # Templated JSON object passed to /Execute
+    data=$( cat <<!
+{
+    "runid" : "$i",
+    "code": "import time;print 1-2;time.sleep(10);print 'hello'",
+    "scrapername": "test",
+    "scraperid": "$i",
+    "language": "python"
+}
+!
+)
     length=${#data}
     set -x
     curl -H "Content-Length: $length" -d "$data" http://127.0.0.1:9001/Execute &
