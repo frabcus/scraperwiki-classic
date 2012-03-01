6d7b255606b06aba21b26de7fe35739b8d935c74
David Jones <david@scraperwiki.com>
2012-03-01 12:30:17 +0000

    added really simple python language scriptmgr execution test

--- services/scriptmgr/tests/testcore.py
+++ services/scriptmgr/tests/testcore.py
@@ -3,14 +3,41 @@
 
 """Tests the core functionality of scriptmgr."""
 
+# http://docs.python.org/release/2.6.7/library/json.html
+import json
 # http://docs.python.org/release/2.6.7/library/unittest.html
 import unittest
 # http://docs.python.org/release/2.6.7/library/urllib.html
 import urllib
 
 class testCore(unittest.TestCase):
+
+    url = "http://127.0.0.1:9001/"
+
     def setUp(self):
-        self.url = "http://127.0.0.1:9001/"
+        pass
+
+    def Execute(self, code, language='python'):
+        """Execute a script on the configured scriptmgr.
+        Potentionally of some general use; could move.
+        """
+
+        # http://docs.python.org/release/2.6.7/library/uuid.html
+        import uuid
+
+        # A random UUID.
+        id = str(uuid.uuid4())
+
+        d = {
+            "runid" : id,
+            "code": code,
+            "scrapername": "test",
+            "scraperid": id,
+            "language": language
+        }
+        body = json.dumps(d)
+        u = urllib.urlopen(self.URL("Execute"), data=body)
+        return u.read()
 
     def URL(self, command):
         """Form a URL to access the scriptmgr by prefixing with
@@ -21,4 +48,8 @@ class testCore(unittest.TestCase):
 
     def testAlive(self):
         urllib.urlopen(self.URL("Status")).read()
+        pass
+    
+    def testPython(self):
+        self.Execute("""print 'hell'+'o'*3""", language='python')
         pass
