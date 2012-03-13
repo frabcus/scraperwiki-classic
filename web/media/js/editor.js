1846c42afbb01a12bc1964668874b4f7f10c6fda
francis <francis@flourish.org>
2012-03-13 09:53:07 +0000

    New wording

--- web/media/js/editor.js
+++ web/media/js/editor.js
@@ -592,7 +592,7 @@ $(document).ready(function()
 
         //clear the tabs
         clearOutput();
-        writeToConsole('*** Job advert! Write scrapers for ScraperWiki in Liverpool: http://bit.ly/z6y16C ***'); 
+        writeToConsole('*** Want to do this for a living? ScraperWiki is hiring: http://bit.ly/z6y16C ***'); 
         writeToConsole('Starting run ... ' + (isstaff ? " ["+lxc+"]" : "")); 
         writeToChat('<i>' + lchatname + ' runs scraper</i>'); 
 
