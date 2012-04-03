4298f7cf860bca0ef8083a41dcbb5d7758de8f77
zarino <devnull@localhost>
2012-04-03 16:14:59 +0100

    fixed duplicate error message issue when adding users to a vault

--- web/media/js/scraperwiki.js
+++ web/media/js/scraperwiki.js
@@ -505,66 +505,66 @@ $(function(){
                         $(e.target).not('[class*="ui-"]').length) {
                             // they didn't click on the users link or the popover or the autocomplete
                             clean_up_users_popover($p);
-                        }
-                    
-			});
-		});
+                    }
+    			});
+    		});
         }
 	});
 	
-	$('body.vaults a.add_user').bind('click', function(){		
-		$('input.username').bind('keydown', function(e){
-			// handle Enter/Return key as a click on the Add button
-			if((e.keyCode || e.which) == 13){
-				$(this).next('a').trigger('click');
-			}
-		}).autocomplete({
-			minLength: 2,
-			source: function( request, response ) {
-				$.ajax({
-					url: $('#id_api_base').val() + "scraper/usersearch",
-					dataType: "jsonp",
-					data: {
-						format:"jsondict", 
-						maxrows:10, 
-						searchquery:request.term
-					},
-					success: function( data ) {
-						response( $.map( data, function( item ) {
-							return {
-								label: item.profilename + ' (' + item.username + ')',
-								value: item.username
-							}
-						}));
-					}
-				});
-			},
-			select: function( event, ui ) {
-				//	submit the name
-				//	$(this).next('a').trigger('click');
-			}
-		}).next().bind('click', function(){
-			var closure = $(this).parents('div.vault_users_popover');
-			$('.error', closure).slideUp(150);
-			var username = $('.username', closure).val();
-			var vault_id = $('a.add_user', closure).attr('rel');
-			var url = '/vaults/' + vault_id + '/adduser/' + username + '/';
-			$.getJSON(url, function(data) {
-				if(data.status == 'ok'){
-					$('.username', closure).autocomplete("close");
-					$('ul', closure).append(data.fragment).next('a').delay(50).slideDown(150);
-					closure.updateUserCount();
-				} else if(data.status == 'fail'){
-					$('ul', closure).append('<li class="message error">' + data.error + '</li>');
-					$('.username', closure).autocomplete("close");
-				} else if(data.status == 'invited'){
-					$('ul', closure).append('<li class="message invite">' + data.message + '</li>');
-					$('.username', closure).autocomplete("close");
-                    $('input.username').val('');
+	$('body.vaults a.add_user').bind('click', function(){
+		$(this).slideUp(250).next().slideDown(250).find('.username').focus();
+	});
+	
+	$('input.username').bind('keydown', function(e){
+		// handle Enter/Return key as a click on the Add button
+		if((e.keyCode || e.which) == 13){
+			$(this).next('a').trigger('click');
+		}
+	}).autocomplete({
+		minLength: 2,
+		source: function( request, response ) {
+			$.ajax({
+				url: $('#id_api_base').val() + "scraper/usersearch",
+				dataType: "jsonp",
+				data: {
+					format:"jsondict", 
+					maxrows:10, 
+					searchquery:request.term
+				},
+				success: function( data ) {
+					response( $.map( data, function( item ) {
+						return {
+							label: item.profilename + ' (' + item.username + ')',
+							value: item.username
+						}
+					}));
 				}
 			});
+		},
+		select: function( event, ui ) {
+			//	submit the name
+			//	$(this).next('a').trigger('click');
+		}
+	}).next().bind('click', function(){
+		var closure = $(this).parents('div.vault_users_popover');
+		$('.error', closure).slideUp(150);
+		var username = $('.username', closure).val();
+		var vault_id = $('a.add_user', closure).attr('rel');
+		var url = '/vaults/' + vault_id + '/adduser/' + username + '/';
+		$.getJSON(url, function(data) {
+			if(data.status == 'ok'){
+				$('.username', closure).autocomplete("close");
+				$('ul', closure).append(data.fragment).next('a').delay(50).slideDown(150);
+				closure.updateUserCount();
+			} else if(data.status == 'fail'){
+				$('ul', closure).append('<li class="message error">' + data.error + '</li>');
+				$('.username', closure).autocomplete("close");
+			} else if(data.status == 'invited'){
+				$('ul', closure).append('<li class="message invite">' + data.message + '</li>');
+				$('.username', closure).autocomplete("close");
+                $('input.username').val('');
+			}
 		});
-		$(this).slideUp(250).next().slideDown(250).find('.username').focus();
 	});
 	
 	
