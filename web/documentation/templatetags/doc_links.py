41bbe0465b5a7b845298106dc9048cef15a1febb
zarino <devnull@localhost>
2012-04-04 11:26:31 +0100

    new HTML for docindex page

--- web/documentation/templatetags/doc_links.py
+++ web/documentation/templatetags/doc_links.py
@@ -16,6 +16,15 @@ def doc_link_full(template_name, language, title = None, text = None):
         return '''<a href="/docs/%s/%s">%s</a>''' % (language, template_name, text)
 
 @register.simple_tag
+def doc_link_toc(template_name, language, description = None, text = None):
+    template_name = template_name.replace('LANG', language)
+    text = text or page_titles[template_name][0]
+    html = '''<dt><a href="/docs/%s/%s">%s</a></dt>''' % (language, template_name, text)
+    if description:
+        html += '''<dd><a href="/docs/%s/%s">%s</a></dd>''' % (language, template_name, description)
+    return html
+
+@register.simple_tag
 def doc_change_lang(request, from_lang, to_lang):
     return request.path.replace(from_lang, to_lang)
 
