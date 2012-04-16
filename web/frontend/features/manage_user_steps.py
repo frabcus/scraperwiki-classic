eaad2853a9ed36ca7b10c50ad6d451cdf284466a
David Jones <david@scraperwiki.com>
2012-04-16 15:11:55 +0100

    added (failing) test for user with no scrapers

--- web/frontend/features/manage_user_steps.py
+++ web/frontend/features/manage_user_steps.py
@@ -6,10 +6,23 @@ def profile_page(step):
     step.behave_as("""
         And I am on the contact page
         """)
-    world.browser.visit(django_url('/profiles/test'))
+    # find URL to profile page
+    el = world.browser.find_by_xpath(
+      "//a[contains(@href, '/profiles/')]").first
+    world.browser.visit(el['href'])
 
 @step('Then I should be on my edit profile page')
 def edit_profile_page(step):
     assert 'profile' in world.browser.url
     assert 'edit' in world.browser.url
 
+@step('Given I am a user with no scrapers')
+def user_no_scrapers(step):
+    # Not logged in.
+    world.browser.visit(django_url('/profiles/subject/'))
+    assert world.browser.is_text_present('no scrapers')
+    # As of 2012-04-16 user 'subject' has no scrapers.
+    step.behave_as("""
+    Given user "subject" with password "pass" is logged in
+    """)
+    
