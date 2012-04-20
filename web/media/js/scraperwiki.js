270e6701d46008529c7560d69f0cd3737c8c66d7
francis <francis@flourish.org>
2012-04-21 00:25:21 +0100

    Remove setCookie/getCookie functions (which have suspicious potential naming clash with part of the CSRF code), and use jQuery cookie instead. Set a path when setting the premium alert cookies, so same on all pages.

--- web/media/js/scraperwiki.js
+++ web/media/js/scraperwiki.js
@@ -257,26 +257,6 @@ function newAlert(htmlcontent, level, actions, duration, onclose){
 	
 }
 
-function setCookie(c_name,value,exdays){
-	var exdate = new Date();
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value = escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-	document.cookie = c_name + "=" + c_value;
-}
-
-function getCookie(c_name){
-	var i,x,y,ARRcookies = document.cookie.split(";");
-	for (i=0;i<ARRcookies.length;i++){
-		x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-		y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-		x = x.replace(/^\s+|\s+$/g,"");
-		if (x == c_name){
-			return unescape(y);
-		}
-	}
-}
-
-
 $(function(){
     showPremiumAccountAlert();	
 
@@ -368,23 +348,23 @@ $(function(){
     });
 	
     function showPremiumAccountAlert(){
-        status = typeof(getCookie('premiumBuy')) == 'undefined' ? 'new' : getCookie('premiumBuy')
+        status = typeof($.cookie('premiumBuy')) == 'undefined' ? 'new' : $.cookie('premiumBuy')
         alert_slide_time = status == 'shown' ? 0 : 250
 
         if (status == 'closed' || status == 'clicked') return
 
         newAlert('Get private code and hourly scheduling with our <b>new premium accounts</b>&hellip;', null, {
             'onclick': function() {
-                setCookie("premiumBuy", 'clicked', 365)
+                $.cookie("premiumBuy", 'clicked', { expires: 365, path: "/" })
                 window.location.replace('/pricing/')
                 if(typeof _gaq !== 'undefined'){ _gaq.push(['_trackEvent', 'Upgrade Alert', 'Buy Now']); }
             },
             'text': '<b>Buy one!</b>'
         }, alert_slide_time, function() {
-            setCookie("premiumBuy", 'closed', 365)
+            $.cookie("premiumBuy", 'closed', { expires: 365, path: "/" })
             if(typeof _gaq !== 'undefined'){ _gaq.push(['_trackEvent', 'Upgrade Alert', 'Closed']); }
         });
-        setCookie("premiumBuy", 'shown', 365)
+        $.cookie("premiumBuy", 'shown', { expires: 365, path: "/" })
         if(typeof _gaq !== 'undefined'){ _gaq.push(['_trackEvent', 'Upgrade Alert', 'Shown']); }
     }
 
