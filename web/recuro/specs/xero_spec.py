1a2d4137dc4ff32aec23a36cdeee93aa6035e635
ehg <chris.blower@jumpwire.co.uk>
2012-04-23 17:21:53 +0100

    start of xero integration

--- web/recuro/specs/xero_spec.py
+++ web/recuro/specs/xero_spec.py
@@ -1,3 +1,16 @@
+from recuro import xero
+from recuro.xerokey import *
+
 def it_can_import_xero():
     from recuro import xero
+
+def it_can_authorise_with_xero():
+    session = xero.authorise(XERO_CONSUMER_KEY, XERO_CONSUMER_SECRET, RSA_KEY)
+    assert session.request
+
+def it_can_call_a_xero_function():
+    session = xero.authorise(XERO_CONSUMER_KEY, XERO_CONSUMER_SECRET, RSA_KEY)
+    resp, content = session.request("/Contacts", "GET")
+    print repr(content)
+    assert resp['status'] == '200'
 
---
+++ web/recuro/xero.py
@@ -0,0 +1,63 @@
+from StringIO import StringIO
+from M2Crypto import RSA
+from M2Crypto.BIO import MemoryBuffer
+import oauth2
+import hashlib
+import base64
+
+def authorise(consumer_key, consumer_secret, rsa_key):
+    return XeroPrivateClient(consumer_key, consumer_secret, rsa_key)
+
+############################################################################
+# Helpers
+
+class SignatureMethod_RSA(oauth2.SignatureMethod):
+    """ RSA signature not implemented by oauth2."""
+    name = "RSA-SHA1"
+
+    def __init__(self, key):
+        super(oauth2.SignatureMethod, self).__init__()
+        self.RSA = RSA.load_key_bio(MemoryBuffer(key))
+
+    def signing_base(self, request):
+        """Calculates the string that needs to be signed."""
+        sig = (
+            oauth2.escape(request.method),
+            oauth2.escape(request.normalized_url),
+            oauth2.escape(request.get_normalized_parameters()),
+        )
+        raw = '&'.join(sig)
+        return raw
+
+    def sign(self, request, consumer, token):
+        """Returns the signature for the given request.
+        Note: consumer and token are not used, but are there to fit in with
+        call in oauth2 module.
+        """
+        raw = self.signing_base(request)
+        digest = hashlib.sha1(raw).digest()
+        signature = self.RSA.sign(digest, algo="sha1")
+        encoded = base64.b64encode(signature)
+        return encoded
+
+class XeroPrivateClient(oauth2.Client):
+    """
+    Xero client for Private Application integration
+    """
+    def __init__(self, consumer_key, consumer_secret,
+                rsa_key, proxy_host=None, proxy_port=None):
+        if proxy_host and proxy_port:
+            proxy_info = httplib2.ProxyInfo(socks.PROXY_TYPE_HTTP, proxy_host,
+                     proxy_port)
+        else:
+            proxy_info = None
+        consumer = oauth2.Consumer(consumer_key, consumer_secret)
+        # For private applications, the consumer key and secret are used as the
+        # access token and access secret.
+        token = oauth2.Token(consumer_key, consumer_secret)
+        oauth2.Client.__init__(self, consumer, token, proxy_info=proxy_info)
+        self.set_signature_method(SignatureMethod_RSA(rsa_key))
+
+    def request(self, path, method='GET'):
+        sup = super(XeroPrivateClient, self)
+        return sup.request("https://api.xero.com/api.xro/2.0%s" % path, method)
