08d035661efd74c99a63af19414695f035344920
ehg <chris.blower@jumpwire.co.uk>
2012-04-24 14:28:00 +0100

    refactored keys into settings file

--- web/recuro/specs/xero_spec.py
+++ web/recuro/specs/xero_spec.py
@@ -1,15 +1,19 @@
 from recuro import xero
-from recuro.xerokey import *
+from django.conf import settings
 
 def it_can_import_xero():
     from recuro import xero
 
 def it_can_authorise_with_xero():
-    session = xero.authorise(XERO_CONSUMER_KEY, XERO_CONSUMER_SECRET, RSA_KEY)
+    session = xero.authorise(settings.XERO_CONSUMER_KEY,
+                             settings.XERO_CONSUMER_SECRET,
+                             settings.XERO_RSA_KEY)
     assert session.request
 
 def it_can_call_a_xero_function():
-    session = xero.authorise(XERO_CONSUMER_KEY, XERO_CONSUMER_SECRET, RSA_KEY)
+    session = xero.authorise(settings.XERO_CONSUMER_KEY,
+                             settings.XERO_CONSUMER_SECRET,
+                             settings.XERO_RSA_KEY)
     resp, content = session.request("/Contacts", "GET")
     print repr(content)
     assert resp['status'] == '200'
