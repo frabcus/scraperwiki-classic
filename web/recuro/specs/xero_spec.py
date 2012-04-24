dd6abc1dde135e54f9850d277915ff0b257ffca6
David Jones <david@scraperwiki.com>
2012-04-24 15:17:26 +0100

    contact_add method for xero

--- web/recuro/specs/xero_spec.py
+++ web/recuro/specs/xero_spec.py
@@ -1,36 +1,32 @@
-import urllib
-
 from recuro import xero
 from django.conf import settings
+
+session = None
+def setup():
+    global session
+    session = xero.XeroPrivateClient(settings.XERO_CONSUMER_KEY,
+                             settings.XERO_CONSUMER_SECRET,
+                             settings.XERO_RSA_KEY)
 
 def it_can_import_xero():
     from recuro import xero
 
 def it_can_authorise_with_xero():
-    session = xero.authorise(settings.XERO_CONSUMER_KEY,
-                             settings.XERO_CONSUMER_SECRET,
-                             settings.XERO_RSA_KEY)
-    assert session.request
+    assert session
 
 def it_can_call_a_xero_function():
-    session = xero.authorise(settings.XERO_CONSUMER_KEY,
-                             settings.XERO_CONSUMER_SECRET,
-                             settings.XERO_RSA_KEY)
-    resp, content = session.request("/Contacts")
+    # Is allowed access to internal _request method; it's
+    # irrelevant which method gets called.  Don't think that this
+    # allowance extends to you.
+    resp, content = session._xero_request('/Contacts')
     print repr(content)
     assert resp['status'] == '200'
 
 def it_can_add_a_contact():
-    session = xero.authorise(settings.XERO_CONSUMER_KEY,
-                             settings.XERO_CONSUMER_SECRET,
-                             settings.XERO_RSA_KEY)
-    # From http://blog.xero.com/developer/api/Contacts/#POST
     body="""<Contact>
       <Name>Test Testerson</Name>
     </Contact>
     """
-    body = urllib.urlencode(dict(xml=body))
-    resp, content = session.request("/Contacts", body=body,
-      method="POST")
+    resp, content = session.contact_add(body)
     print repr(content)
     assert resp['status'] == '200'
