469e189f7625cf38d5ecbfb1933a8ec2f8fe1731
ehg <chris.blower@jumpwire.co.uk>
2012-04-25 14:36:36 +0100

    recurly just post raw xml

--- web/recuro/views.py
+++ web/recuro/views.py
@@ -4,5 +4,5 @@ from recuro import recurly_parser
 
 @csrf_exempt
 def notify(request):
-    recurly_parser.parse(request.POST['body'])
+    recurly_parser.parse(request.raw_post_data)
     return HttpResponse("ok", mimetype="text/plain")
