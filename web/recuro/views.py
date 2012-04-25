eafd5d193cf80680e26da53c238e15babd8989b8
ehg <chris.blower@jumpwire.co.uk>
2012-04-25 14:22:43 +0100

    make notify view CRSF exempt

--- web/recuro/views.py
+++ web/recuro/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from recuro import recurly_parser
 
+@csrf_exempt
 def notify(request):
     recurly_parser.parse(request.POST['body'])
+    return HttpResponse("ok", mimetype="text/plain")
