18f4d27555025c0afd73bc9eae84bafdcd9399a0
Thomas Levine <perluette@thomaslevine.com>
2012-05-22 06:13:49 -0400

    move test settings to settings-test

--- web/settings-test.py
+++ web/settings-test.py
@@ -1,9 +1,13 @@
 from settings import *
 
-DATABASES['default']['NAME'] = 'test-database.db'
+# Ways of speeding up tests.
+# See: http://stackoverflow.com/questions/3096148/how-to-run-djangos-test-database-only-in-memory
 DATABASES['default']['ENGINE'] = 'sqlite3'
+# DATABASES['default']['NAME'] = 'test-database.db'
+DATABASES['default']['TEST_NAME'] = ':memory:'
+
 # Any options specified are going to be MySQL specific.
-del DATABASES['default']['OPTIONS']
+del(DATABASES['default']['OPTIONS'])
 SOUTH_TESTS_MIGRATE = False
 OVERDUE_SQL = "julianday(strftime(?,last_run) + run_interval,'unixepoch') < julianday('now', 'localtime') or last_run is null"
 OVERDUE_SQL_PARAMS = ['%s']
@@ -11,4 +15,3 @@ LETTUCE_TESTING = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/sw_lettuce_terrain_emails'
-
