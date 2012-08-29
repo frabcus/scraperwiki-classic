082a7e983eb8892169c6b47f4ca0e70addce32cb
David Jones <david@scraperwiki.com>
2012-08-29 15:21:59 +0100

    refactor: removed p[0]

--- web/froth/views.py
+++ web/froth/views.py
@@ -9,13 +9,14 @@ def check_key(request, apikey):
     if len(p) == 0:
         status = 403 # Forbidden
         content = {'error':'Forbidden'}
-    elif p[0].user.is_staff:
-        status = 200 # Okay
-    elif p[0].plan == 'free':
-        status = 402 # Pay
-        content = {'error':'Pay'}
     else:
-        status = 200 # Okay
-    if status == 200:
-        content = {'org':p[0].user.username}
+        profile = p[0]
+        content = {'org':profile.user.username}
+        if profile.user.is_staff:
+            status = 200 # Okay
+        elif profile.plan == 'free':
+            status = 402 # Pay
+            content = {'error':'Pay'}
+        else:
+            status = 200 # Okay
     return HttpResponse(json.dumps(content), mimetype="text/plain", status=status)
