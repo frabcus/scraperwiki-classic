476aa016ed706c0bd3a04ba398613734989e9b88
zarino <devnull@localhost>
2012-12-03 12:36:29 +0000

    removed black data services nav styling, and made data services supernav link clickable

--- web/media/js/scraperwiki.js
+++ web/media/js/scraperwiki.js
@@ -284,7 +284,7 @@ $(function(){
         }
     });
     
-    $('.supernav li a').not('.signup a').bind('click', function(e){
+    $('.supernav li a').not('li.signup a, body:not(.dataservices) li.dataservices a').bind('click', function(e){
         e.preventDefault();
         var $li = $(this).parent();
         var $sub = $('.subnav.' + $li.attr('class').split(" ")[0]);
