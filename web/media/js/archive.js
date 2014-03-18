c68c78532199d82a47c1f2d19d830923145adcc7
zarino <devnull@localhost>
2014-03-18 16:27:08 +0000

    archive.js message for all docs pages

--- web/media/js/archive.js
+++ web/media/js/archive.js
@@ -17,25 +17,36 @@ You add a coloured background to any placeholder by adding a class of "yellow", 
 
 */
 
+var code_in_browser_url = 'https://scraperwiki.com/tools/code-in-browser'
+var scraping_docs_url = 'https://github.com/frabcus/code-scraper-in-browser-tool/wiki'
+
 var archive_messages = {
   docs_api: function(){
     $('#archive_placeholder_top')
       .addClass('yellow')
-      .html('<h3>The ScraperWiki Classic Web API has been retired.</h3><p>This page has been retained for posterity. New users should try our <a href="https://scraperwiki.com/tools/code-in-browser">Code in your browser</a> tool.</p>')
+      .html('<h3>The ScraperWiki Classic Web API has been retired.</h3><p>This page has been retained for posterity. New users should try our <a href="' + code_in_browser_url + '">Code in your browser</a> tool.</p>')
       .show()
   },
   docs_home: function(){
     $('#archive_placeholder_top')
       .addClass('orange')
-      .html('<h3>ScraperWiki Classic has been retired.</h3><p>This documentation has moved to a <a href="https://github.com/frabcus/code-scraper-in-browser-tool/wiki">wiki on Github</a>.</p>')
+      .html('<h3>ScraperWiki Classic has been retired.</h3><p>This documentation has moved to a <a href="' + scraping_docs_url + '">wiki on Github</a>.</p>')
+      .show()
+  },
+  docs_general: function(){
+    $('#archive_placeholder_top')
+      .addClass('yellow')
+      .html('<h3>ScraperWiki Classic has been retired.</h3><p>Why not try our new <a href="' + code_in_browser_url + '">Code in your browser</a> tool? It&rsquo;s free and awesome!</p>')
       .show()
   }
 }
 
 $(function(){
-  if(/docs[/]api/.test(window.location.pathname)){
+  if (/^[/]docs[/]api/.test(window.location.pathname)) {
     archive_messages.docs_api()
-  } else if(/docs[/](python|ruby|php)[/]?/.test(window.location.pathname)){
+  } else if (/^[/]docs[/](python|ruby|php)[/]?$/.test(window.location.pathname)) {
     archive_messages.docs_home()
+  } else if (/^[/]docs[/]/.test(window.location.pathname)) {
+    archive_messages.docs_general()
   }
 })
