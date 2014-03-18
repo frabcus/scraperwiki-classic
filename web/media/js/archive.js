b43ace084e60ce0ba674cefa959e78924680249f
zarino <devnull@localhost>
2014-03-18 16:20:15 +0000

    use archive.js for documentation message

--- web/media/js/archive.js
+++ web/media/js/archive.js
@@ -21,7 +21,13 @@ var archive_messages = {
   docs_api: function(){
     $('#archive_placeholder_top')
       .addClass('yellow')
-      .html('<p style="font-weight:bold">The ScraperWiki Classic Web API has been retired.</p><p>This page has been retained for posterity. New users should try our <a href="https://scraperwiki.com/tools/code-in-browser">Code in your browser</a> tool.</p>')
+      .html('<h3>The ScraperWiki Classic Web API has been retired.</h3><p>This page has been retained for posterity. New users should try our <a href="https://scraperwiki.com/tools/code-in-browser">Code in your browser</a> tool.</p>')
+      .show()
+  },
+  docs_home: function(){
+    $('#archive_placeholder_top')
+      .addClass('orange')
+      .html('<h3>ScraperWiki Classic has been retired.</h3><p>This documentation has moved to a <a href="https://github.com/frabcus/code-scraper-in-browser-tool/wiki">wiki on Github</a>.</p>')
       .show()
   }
 }
@@ -29,5 +35,7 @@ var archive_messages = {
 $(function(){
   if(/docs[/]api/.test(window.location.pathname)){
     archive_messages.docs_api()
+  } else if(/docs[/](python|ruby|php)[/]?/.test(window.location.pathname)){
+    archive_messages.docs_home()
   }
 })
